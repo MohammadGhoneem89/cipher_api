a41e6d9e17c7e0e350a406f812b8173254b0cd7f
Arham <arham.ali@avanzasolutions.com>
2018-05-17 15:46:29 +0500

    .

--- scripts/encrypt.js
+++ scripts/encrypt.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 const _ = require('lodash');
 
 function encrypt() {
-  const str = _.get(process, 'argv[2]', 'abcdefg1234567890!@#$%^&*()');
+  const str = _.get(process, 'argv[2]', 'hello world');
   const encryptStr = _encrypt(str);
   console.log({ encrypt: encryptStr }); //eslint-disable-line
 }
