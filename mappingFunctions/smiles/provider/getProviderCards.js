26038d0b67d4c0ea132fe14f626ac21c470130e6
Mohammed Rashid <mohmad.rashid@hotmail.com>
2018-05-21 14:18:26 +0400

    Final

---
+++ mappingFunctions/smiles/provider/getProviderCards.js
@@ -0,0 +1,36 @@
+'use strict';
+var config = require('../../api/bootstrap/smiles.json')
+const rp = require('request-promise');
+const logger = require('../../lib/helpers/logger')().app;
+
+function format(data) {
+    return {
+        providerInfo: {
+            action: 'providerInfo',
+            data: data
+        }
+    }
+}
+
+module.getProviderCards = function(payload, UUIDKey, route, callback, JWToken) {
+    let URL = config['host'] + '/provider/info';
+    var options = {
+        method: 'POST',
+        uri: URL,
+        body: payload,
+        json: true // Automatically stringifies the body to JSON
+    };
+    logger.info("The notification going is as follows" + JSON.stringify(payload))
+
+    rp(options)
+        .then(function (parsedBody) {
+            logger.debug(JSON.stringify(parsedBody));
+            logger.debug('==================== Sent Successfully==================');
+            const formattedData = format(parsedBody);
+            callback(formattedData);
+        })
+        .catch(function (err) {
+            // POST failed...
+            logger.debug('==================== Request Failed==================' + err);
+        });
+}
