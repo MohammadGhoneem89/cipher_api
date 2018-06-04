f0c836bc785ed6370a65cef890e3a7e1b2412ad7
Mohammed Rashid <mohmad.rashid@hotmail.com>
2018-06-04 12:17:14 +0400

    Tests

--- mappingFunctions/smiles/general/viewItem.js
+++ mappingFunctions/smiles/general/viewItem.js
@@ -2,15 +2,18 @@
 var config = require('../../../api/bootstrap/smiles.json')
 const rp = require('request-promise');
 const logger = require('../../../lib/helpers/logger')().app;
-const category = [ "None", "New Offers", "Trending Offers", "Shopping Offers", "Dining Offers", "Entertainment Offers", "Wellness Offers", "Travel Offers", "Etisalat Services", "Financial", "Other", "Shopping" ];
-const subcategory = {"3":{"31":"Malls & Hypermarkets","32":"Fashion & Jewellery","33":"Home & Lifestyle","34":"Services","35":"Gifts","36":"Services"},"4":{"41":"Cafes","42":"Family & Casual","43":"Fine Dining"},"5":{"51":"SEE","52":"DO","53":"Family & Casual","54":"Sports and Leisure","55":"Theme Parks"},"6":{"61":"Health","62":"Grooming","63":"Beauty","64":"Training","65":"Charity","66":"Learning"},"7":{"71":"Airlines","72":"Hotels","73":"Holidays","74":"Car Hire","75":"Hotel"},"8":{"81":"Voice Package","82":"Data Package","83":"Roaming Package","86":"SMS Package","87":"Free Credit","88":"Free Minutes","90":"Recharge Credit","91":"Etisalat Bundle","92":"Smiles Offer","93":"Data Offer","94":"International Minutes","95":"Local Minutes","96":"Combo Pack","97":"Local/International Minutes","98":"SMS Offer Object","99":"Free International Minutes","100":"Free SMS Offer","101":"Data Deal","102":"Minutes Deal","103":"SMS Deal","104":"Combo Offer","105":"National Minutes","106":"Internet Calling Plan"},"11":{"111":"Fashion & Jewellery"}};
+const category = ["None", "New Offers", "Trending Offers", "Shopping Offers", "Dining Offers", "Entertainment Offers", "Wellness Offers", "Travel Offers", "Etisalat Services", "Financial", "Other", "Shopping"];
+const subcategory = { "3": { "31": "Malls & Hypermarkets", "32": "Fashion & Jewellery", "33": "Home & Lifestyle", "34": "Services", "35": "Gifts", "36": "Services" }, "4": { "41": "Cafes", "42": "Family & Casual", "43": "Fine Dining" }, "5": { "51": "SEE", "52": "DO", "53": "Family & Casual", "54": "Sports and Leisure", "55": "Theme Parks" }, "6": { "61": "Health", "62": "Grooming", "63": "Beauty", "64": "Training", "65": "Charity", "66": "Learning" }, "7": { "71": "Airlines", "72": "Hotels", "73": "Holidays", "74": "Car Hire", "75": "Hotel" }, "8": { "81": "Voice Package", "82": "Data Package", "83": "Roaming Package", "86": "SMS Package", "87": "Free Credit", "88": "Free Minutes", "90": "Recharge Credit", "91": "Etisalat Bundle", "92": "Smiles Offer", "93": "Data Offer", "94": "International Minutes", "95": "Local Minutes", "96": "Combo Pack", "97": "Local/International Minutes", "98": "SMS Offer Object", "99": "Free International Minutes", "100": "Free SMS Offer", "101": "Data Deal", "102": "Minutes Deal", "103": "SMS Deal", "104": "Combo Offer", "105": "National Minutes", "106": "Internet Calling Plan" }, "11": { "111": "Fashion & Jewellery" } };
 
 
 function format(data) {
     const categoryID = data.data.CATEGORY_ID;
-    data.data.CATEGORY_ID = category[categoryID]||"Not Defined";
-    if(categoryID&&subcategory[categoryID])
-    data.data.SUB_CATEGORY_ID = ((subcategory[data.data.CATEGORY_ID])[data.data.SUB_CATEGORY_ID])||"Not Defined";
+    data.data.CATEGORY_ID = category[categoryID] || "Not Defined";
+    if (categoryID && subcategory[categoryID]) {
+        console.log(categoryID, subcategory(categoryID))
+        data.data.SUB_CATEGORY_ID = ((subcategory[data.data.CATEGORY_ID])[data.data.SUB_CATEGORY_ID]) || "Not Defined";
+    }
+
 
     return {
         action: 'viewItem',
