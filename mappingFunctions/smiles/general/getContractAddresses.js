4fc70fe21a729e347aa6080c00d4644a50a51387
Mohammed Rashid <mohmad.rashid@hotmail.com>
2018-06-04 18:21:29 +0400

    Final

--- mappingFunctions/smiles/general/getContractAddresses.js
+++ mappingFunctions/smiles/general/getContractAddresses.js
@@ -2,20 +2,25 @@
 var config = require('../../../api/bootstrap/smiles.json')
 const rp = require('request-promise');
 const logger = require('../../../lib/helpers/logger')().app;
+const smilesContract = require('../../lib/services/smilesContracts');
 
 const contractAddresses = [{
     name: 'Merchant',
     value: '0x25fD7831ccc5F4d7124e0579e89495420d82b5E0'
 }]
+
 exports.getContractAddresses = function (payload, UUIDKey, route, callback, JWToken) {
 
     logger.info("The notification going is as follows" + JSON.stringify(payload))
+    smilesContract.getAll().then(data=>{
+        callback({
+            "contracts": {
+                "data": data
+            }
+        });
+    })
 
-    callback({
-        "contracts": {
-            "data": contractAddresses
-        }
-    });
+   
 }
 
 
