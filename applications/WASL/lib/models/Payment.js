b157d43cd55f6ef42f3ed477f85b40cbff4ab0ba
Sandeep Kumar <sandeep.kumar@avanzasolutions.com>
2018-12-25 19:46:44 +0400

    kashan changes

--- applications/WASL/lib/models/Payment.js
+++ applications/WASL/lib/models/Payment.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:applications/WASL/lib/models/Payment.js
 'use strict';
 
 const mongoose = require('mongoose');
@@ -6,82 +5,38 @@ const Schema = mongoose.Schema;
 const dates = require('../../../../lib/helpers/dates');
 
 const schema = new Schema({
-    name: {
-        type: String
+  name: {
+    type: String
+  },
+  code: {
+    type: String
+  },
+  beneficiaryInfo: [{
+    key: {
+      type: String
     },
-    code: {
-        type: String
-    },
-    beneficiaryInfo: [
-        {
-            key: {
-                type: String
-            },
-            value: {
-                type: String
-            }
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: dates.newDate
-    },
-    updatedAt: {
-        type: Date,
-        default: dates.newDate
-    },
-    createdBy: {
-        type: Schema.Types.ObjectId, ref: 'User'
-    },
-    updatedBy: {
-        type: Schema.Types.ObjectId, ref: 'User'
+    value: {
+      type: String
     }
+  }],
+  createdAt: {
+    type: Date,
+    default: dates.newDate
+  },
+  updatedAt: {
+    type: Date,
+    default: dates.newDate
+  },
+  createdBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  updatedBy: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 
 });
 const Payment = mongoose.model('Payment', schema, 'Payment');
 
 module.exports = Payment;
-=======
-'use strict';
-
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const dates = require('../../../../lib/helpers/dates');
-
-const schema = new Schema({
-    name: {
-        type: String
-    },
-    code: {
-        type: String
-    },
-    beneficiaryInfo: [
-        {
-            key: {
-                type: String
-            },
-            value: {
-                type: String
-            }
-        }
-    ],
-    createdAt: {
-        type: Date,
-        default: dates.newDate
-    },
-    updatedAt: {
-        type: Date,
-        default: dates.newDate
-    },
-    createdBy: {
-        type: Schema.Types.ObjectId, ref: 'User'
-    },
-    updatedBy: {
-        type: Schema.Types.ObjectId, ref: 'User'
-    }
-
-});
-const Payment = mongoose.model('Payment', schema, 'Payment');
-
-module.exports = Payment;
->>>>>>> origin/wasl_property_project:applications/WASL/lib/models/Payment.js
