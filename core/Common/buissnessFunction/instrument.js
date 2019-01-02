719478e8257832530c99e0d110ba067578c2c57b
Bilal Maroof <bilal.maroof@avanzainnovations.com>
2019-01-02 20:55:47 +0400

    api changes 1

--- core/Common/buissnessFunction/instrument.js
+++ core/Common/buissnessFunction/instrument.js
@@ -8,7 +8,7 @@ module.exports = {
     if (!(data instanceof Array)) {
       throw new Error("Instrumentation Field(s) missing");
     }
-    data.forEach((element,index) => {
+    data.forEach((element, index) => {
       if (!element.bankCode || !element.paymentMethod || !element.date || !element.amount) {
         throw new Error(`Mandotary Field(s) missing from array on index ${index}`);
       }
@@ -18,6 +18,14 @@ module.exports = {
       element.bankMetaData = element.bankMetaData ? JSON.stringify(element.bankMetaData) : undefined;
       element.beneficiaryData = element.beneficiaryData ? JSON.stringify(element.beneficiaryData) : undefined;
     });
+    return data;
+  },
+  validateInstrumentObject: (data, payload, jwt) => {
+    data.date = dates.ddMMyyyyslash(data.date);
+    data.amount = parseFloat(data.amount) || 0;
+    data.providerMetaData = data.providerMetaData ? JSON.stringify(data.providerMetaData) : undefined;
+    data.bankMetaData = data.bankMetaData ? JSON.stringify(data.bankMetaData) : undefined;
+    data.beneficiaryData = data.beneficiaryData ? JSON.stringify(data.beneficiaryData) : undefined;
     return data;
   }
 
