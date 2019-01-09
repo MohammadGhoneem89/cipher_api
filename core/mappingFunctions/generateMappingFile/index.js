430aae549dee74ae512f9d20a37f6538ac61d862
Mohammed Rashid <mohmad.rashid@hotmail.com>
2019-01-09 14:58:49 +0400

    Refractored the code to split into multiple file

---
+++ core/mappingFunctions/generateMappingFile/index.js
@@ -0,0 +1,56 @@
+const generateFileContent = require('./writeToFile')
+const postgres = require('./DBScriptGenerators/postgress');
+
+const generateMappingFile = async function (payload, UUIDKey, route, callback, JWToken) {
+    let file;
+    try {
+        switch (payload.database) {
+            case 'postgres':
+                file = postgres(payload);
+                break;
+            case 'mongo':
+                break;
+            default:
+                throw 'Not found';
+        }
+        generateFileContent(file, payload.enableActions, callback);
+    } catch (err) {
+        //logger.debug(" [ DB ] ERROR : " + err);
+        console.log(err)
+        callback({
+            error: err
+        })
+    }
+}
+
+
+let data = {
+    "database": "postgres",
+    "adaptor":"adaptor4",
+    "enableActions": true,
+    "enablePaging": true,
+    "objectType": "tables",
+    "object": "invoiceResponse",
+    "conditions": [
+        {
+            "name": "suppliername",
+            "value": "name"
+        },
+        {
+            "name": "type",
+            "value": "type"
+        }
+    ],
+    "fields": [
+        {
+            "name": "refNo",
+            "as": "reference"
+        }
+    ]
+}
+
+generateMappingFile(data, '', '', (data) => {
+    console.log(data)
+}, '')
+
+exports.generateMappingFile = generateMappingFile;
---
+++ core/mappingFunctions/generateMappingFile/writeToFile.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const uuid = require('uuid/v1');
+const path = require('path')
+module.exports = function (file, isActions, callback) {
+    let actions = '';
+    if (isActions) {
+        actions = `for(let i=0;i<response.length;i++){
+            response[i]['actions']=[{
+                "actionType": "COMPONENT_FUNCTION",
+                "iconName": "fa fa-eye",
+                "label": "View",
+                "URI": [
+                    "/"
+                ]
+            }]
+        }`
+    }
+    let overall = `const keyVaultRepo = require('../../../lib/repositories/keyVault');\n
+    const client = require('../../api/client');
+    \nconst execute = async function (payload, UUIDKey, route, callback, JWToken) {
+        try{
+            ${file}
+            \n${actions}
+            callback({
+                dbExecution: {
+                    data:response}});
+        } catch(err){
+            callback({error: err})
+        }
+        \n}
+        \nexports.execute = execute;
+    `;
+    const id = uuid();
+    fs.writeFile(path.join(__dirname, '../', 'generated', `${id}.js`), overall, function (err) {
+        if (err) {
+            callback({
+                error: err
+            })
+        }
+        callback({
+            generateMappingFile: {
+                data: {
+                    uuid: id,
+                    functionName: 'execute',
+                    path: `/generated/${id}.js`
+                },
+                status: 'OK'
+            }
+        })
+    });
+}
