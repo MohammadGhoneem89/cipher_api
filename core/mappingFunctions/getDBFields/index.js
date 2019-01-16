7c975e10ad37f50d15d0124bfeb41bfbc7ea758f
Mohammed Rashid <mohmad.rashid@hotmail.com>
2019-01-16 13:52:45 +0400

    Fixed issue related to table fields

--- core/mappingFunctions/getDBFields/index.js
+++ core/mappingFunctions/getDBFields/index.js
@@ -1,43 +1,25 @@
-const client = require('../../api/client');
 let logger = require('../../../lib/helpers/logger')().app;
-const keyVaultRepo = require('../../../lib/repositories/keyVault');
-const postgresProcess = require('./adaptors/postgres');
+const TableFields = require('../../../lib/models/TableFields');
+
 const getDBFields = async function (payload, UUIDKey, route, callback, JWToken) {
   const response = {
     getDBFields: {
       action: payload.action,
-      data: []
+      data: [],
+      outputFields: []
     }
   };
   try {
-    let dbConfig = await keyVaultRepo.getDBConfig(payload.database, payload.adaptor);
-    let instance = await client.createClient(payload.database, dbConfig.connection);
-    switch (payload.database) {
-      case 'postgres':
-        if (payload.objectType === 'table') {
-          response.getDBFields.data = await postgresProcess(instance, payload);
-        } else {
-          const query = {
-            text: `SELECT  proargnames
-            FROM    pg_catalog.pg_namespace n
-            JOIN    pg_catalog.pg_proc p
-            ON      p.pronamespace = n.oid
-            WHERE   n.nspname = 'public' and p.proname='${payload.object}';`,
-            values: []
-          };
-          let data = await instance.query(query);
-          data = data.rows[0].proargnames;
-          for (let i = 0; i < data.length; i++) {
-            response.getDBFields.data.push({
-              _id: data[i],
-              label: data[i],
-              name: data[i]
-            });
-          }
-        }
-        break;
-      case 'mongo':
-        break;
+    let data = await TableFields.findOne({
+      adaptor: payload.adaptor,
+      name: payload.object,
+      type: payload.objectType
+    }).lean();
+    response.getDBFields.data = data.fields;
+    if (payload.objectType === 'storedProcedure') {
+      response.getDBFields.outputFields = data.outputs;
+    } else {
+      response.getDBFields.outputFields = data.fields;
     }
   } catch (err) {
     console.log(err)
@@ -47,3 +29,37 @@ const getDBFields = async function (payload, UUIDKey, route, callback, JWToken) 
 }
 
 exports.getDBFields = getDBFields;
+
+let table = [
+  {
+    "name" : "block_num",
+    "type" : {
+        "type" : "BIGINT",
+        "allowNull" : false
+    }
+}, 
+{
+    "name" : "txnid",
+    "typeData" : {
+        "type" : "STRING",
+        "allowNull" : false,
+        "validate" : {}
+    }
+}, 
+{
+    "name" : "status",
+    "typeData" : {
+        "type" : "STRING",
+        "allowNull" : false,
+        "defaultValue" : "INVALID"
+    }
+}, 
+{
+    "name" : "key",
+    "typeData" : {
+        "type" : "STRING",
+        "allowNull" : false,
+        "unique" : true
+    }
+}
+]
