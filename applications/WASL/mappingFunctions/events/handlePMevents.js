bc1f6fd4f286a2bcd297e73585d5c1ac727d8a17
ArhamAliQureshi <arham.ali@avanzasolutions.com>
2019-01-21 14:43:40 +0500

    .

--- applications/WASL/mappingFunctions/events/handlePMevents.js
+++ applications/WASL/mappingFunctions/events/handlePMevents.js
@@ -88,7 +88,7 @@ function updatePaymentStatus(payload) {
               username: 'api_user',
               password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
             },
-          body: JSON.parse(transformTemplate(EventOnUpdateFirstPaymentStatus, payload.eventData))
+          body: transformTemplate(EventOnUpdateFirstPaymentStatus, payload.eventData)
           // body: EventOnUpdateFirstPaymentStatus
 
         },
