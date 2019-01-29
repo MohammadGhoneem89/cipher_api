66c8c0f8baea830d60fee25c4715d89da4e0bb9d
ArhamAliQureshi <arham.ali@avanzasolutions.com>
2019-01-29 18:13:52 +0500

    RenewContract Hanlder added

--- applications/WASL/mappingFunctions/events/handleBankevents.js
+++ applications/WASL/mappingFunctions/events/handleBankevents.js
@@ -1,26 +1,27 @@
-
 'use strict';
 let rp = require('request-promise');
 const transformTemplate = require('../../../../lib/helpers/transformTemplate');
+
 async function handleBankevents(payload, UUIDKey, route, callback, JWToken) {
   try {
-    console.log("<<<<<<<<< Request Recieved for Event >>>>>>>>")
-    console.log(JSON.stringify(payload, null, 2), "========> THIS IS PAYLOAD")
+    console.log("<<<<<<<<< Request Recieved for Event >>>>>>>>");
+    console.log(JSON.stringify(payload, null, 2), "========> THIS IS PAYLOAD");
 
     switch (payload.eventData.eventName) {
+      case "RenewContract": {
+        console.log("============RenewContract===================>", payload, "<============RenewContract===================");
+        break;
+      }
 
-      case "ProcessInstrument":
-        {
-          return getPromise(payload, ProcessInstrument, callback)
-        }
-      case "AssociatePaymentInstruments":
-        {
-
-        }
-      case "EventsOnProcessPaymentInstruments":
-        {
-
-        }
+      case "ProcessInstrument": {
+        return getPromise(payload, ProcessInstrument, callback)
+      }
+      case "AssociatePaymentInstruments": {
+        break;
+      }
+      case "EventsOnProcessPaymentInstruments": {
+        break;
+      }
       case "UpdatePaymentInstrumentStatus": {
         try {
           //await UpdateContractStatus(payload);
@@ -30,25 +31,24 @@ async function handleBankevents(payload, UUIDKey, route, callback, JWToken) {
         }
         break;
       }
-    
-      case "EventOnUpdatePaymentStatus":
-    {
 
-    }
-
+      case "EventOnUpdatePaymentStatus": {
+        break;
+      }
 
       default:
-    return callback({
-      error: true,
-      message: "invalid case"
-    })
+        return callback({
+          error: true,
+          message: "invalid case"
+        })
 
-  }
+    }
   }
   catch (err) {
-  console.log(err)
+    console.log(err)
+  }
 }
-}
+
 exports.handleBankevents = handleBankevents
 
 
@@ -63,33 +63,34 @@ function ProcessInstrument() {
     url: 'http://51.140.250.28/API/PR/ProcessInstrument',
 
     body:
-    {
-      header:
       {
-        username: 'waslapi',
-        password: 'aa8dd29e572a64982d7d2bf48325a4951b7c399a1283fb33460ca275e230d5ae308dcd820d808c5ea0d23e047bd2f3e066bf402cb249d989408331566f7ca890'
+        header:
+          {
+            username: 'waslapi',
+            password: 'aa8dd29e572a64982d7d2bf48325a4951b7c399a1283fb33460ca275e230d5ae308dcd820d808c5ea0d23e047bd2f3e066bf402cb249d989408331566f7ca890'
+          },
+        body:
+          {
+            orgCode: 'WASL',
+            contractID: 'DIRC932',
+            paymentInstruments:
+              {
+                paymentMethod: '001',
+                bankCode: 'EI',
+                instrumentID: 'ECHEQUE0004',
+                internalInstrumentID: '1b5745a0-1287-11e9-90bb-cb3b7eba35fe-3',
+                date: '01/01/2018',
+                amount: 20000,
+                status: '005',
+                bankMetaData: {MICR: '1111111111111', paymentID: 'ECHEQUE111', others: 'NA'}
+              }
+          }
       },
-      body:
-      {
-        orgCode: 'WASL',
-        contractID: 'DIRC932',
-        paymentInstruments:
-        {
-          paymentMethod: '001',
-          bankCode: 'EI',
-          instrumentID: 'ECHEQUE0004',
-          internalInstrumentID: '1b5745a0-1287-11e9-90bb-cb3b7eba35fe-3',
-          date: '01/01/2018',
-          amount: 20000,
-          status: '005',
-          bankMetaData: { MICR: '1111111111111', paymentID: 'ECHEQUE111', others: 'NA' }
-        }
-      }
-    },
     json: true
   };
   return rp(options);
 }
+
 function updatePaymentStatus(payload) {
   console.log("PAYLOADY=====================> ", payload.eventData, " <=====================PAYLOADY");
 
@@ -100,16 +101,16 @@ function updatePaymentStatus(payload) {
       method: 'POST',
       url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=paymentStatus',
       body:
-      {
-        header:
         {
-          username: 'api_user',
-          password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
-        },
-        body: await transformTemplate("EventOnUpdatePaymentStatus", payload.eventData, [])
-        // body: EventOnUpdateFirstPaymentStatus
+          header:
+            {
+              username: 'api_user',
+              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
+            },
+          body: await transformTemplate("EventOnUpdatePaymentStatus", payload.eventData, [])
+          // body: EventOnUpdateFirstPaymentStatus
 
-      },
+        },
       json: true
     };
     console.log("REQUEST===============>", options.body, "<===============REQUEST");
@@ -117,6 +118,7 @@ function updatePaymentStatus(payload) {
   }
 
 }
+
 async function getPromise(payload, func, callback) {
   func().then(response => {
     console.log(response, "RESPONSE");
