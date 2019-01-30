aeb2213b33155a001b607d97b3e61a40003e859c
ArhamAliQureshi <arham.ali@avanzasolutions.com>
2019-01-30 21:51:45 +0500

    .

--- applications/WASL/mappingFunctions/events/handlePMevents.js
+++ applications/WASL/mappingFunctions/events/handlePMevents.js
@@ -8,98 +8,50 @@ async function handlePMevents(payload, route, callback, JWToken) {
     console.log("<<<<<<<<< Request Recieved for PM Event >>>>>>>>");
     console.log(JSON.stringify(payload, null, 2));
     console.log(payload.eventData.eventName, "===========================>handlePMevents THIS IS PAYLOAD");
-    // switch (payload.eventData.eventName) {
-    //
-    //   case "UpdateFirstPaymentInstrumentStatus": {
-    //     try {
-    //       await UpdateContractStatus(payload);
-    //       await getPromise(payload, updateFirstPaymentStatus(payload), callback);
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //     break;
-    //   }
-    //   case "UpdatePaymentInstrumentStatus": {
-    //     try {
-    //       await UpdateContractStatus(payload);
-    //       await getPromise(payload, updatePaymentStatus(payload), callback);
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //     break;
-    //   }
-    //
-    //   case "UpdateKYCDetail": {
-    //     try {
-    //       await getPromise(payload, updateKYCDetail(payload), callback);
-    //     }
-    //     catch (e) {
-    //       console.log(e);
-    //     }
-    //     break;
-    //   }
-    //   case "EjariData": {
-    //     try {
-    //       await getPromise(payload, EjariAvailable(payload), callback);
-    //     }
-    //     catch (e) {
-    //       console.log(e);
-    //     }
-    //     break;
-    //   }
-    //   case "EjariTerminationStatus": {
-    //     try {
-    //       await getPromise(payload, EjariTermination(payload), callback);
-    //     } catch (e) {
-    //       console.log(e);
-    //     }
-    //     break;
-    //   }
-    //
-    //   default:
-    //     return callback({
-    //       error: true,
-    //       message: "invalid case"
-    //     })
-    //
-    // }
+    switch (payload.eventData.eventName) {
+      case "UpdateFirstPaymentInstrumentStatus": {
+        await UpdateContractStatus(payload.eventData.contractID);
+        await getPromise(payload, updateFirstPaymentStatus(payload), callback);
+        break;
+      }
+      case "UpdatePaymentInstrumentStatus": {
+        await UpdateContractStatus(payload.eventData.contractID);
+        await getPromise(payload, updatePaymentStatus(payload), callback);
+        break;
+      }
+      default:
+        return callback({
+          error: true,
+          message: "invalid case"
+        })
 
-    callback({
-      error: false,
-      message: payload.eventData.eventName + " Dispatched",
-      response: {request: "THIS IS REQUEST", response:"THIS IS RESPONSE"}
-    })
+    }
   }
   catch (err) {
     console.log(err);
     callback({
       error: true,
-      message: err,
-      response: {request: "THIS IS REQUEST", response:"THIS IS RESPONSE"}
+      message: "ERROR",
+      response: {request: "THIS IS REQUEST", response: err}
     })
   }
 
 }
 
 function updatePaymentStatus(payload) {
-  console.log("PAYLOADY=====================> ", payload.eventData, " <=====================PAYLOADY");
-
   return async () => {
-    console.log("OUTPUT=====================> ", await transformTemplate("EventOnUpdatePaymentStatus", payload.eventData, []), " <=====================OUTPUT");
     let options = {
       method: 'POST',
-      url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=paymentStatus',
+      url: payload.endpoint.address,
       body:
         {
           header:
             {
-              username: 'api_user',
-              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
+              username: payload.header.username,
+              password: payload.header.password
             },
-          body: await transformTemplate("EventOnUpdatePaymentStatus", payload.eventData, [])
-// body: EventOnUpdateFirstPaymentStatus
-
-      },
+          body: await transformTemplate(payload.template.data, payload.eventData, [])
+        },
       json: true
     };
     console.log("REQUEST===============>", options.body, "<===============REQUEST");
@@ -112,91 +64,25 @@ function updateFirstPaymentStatus(payload) {
   return async () => {
     let options = {
       method: 'POST',
-      url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=paymentStatus',
+      url: payload.endpoint.address,
       body:
         {
           header:
             {
-              username: 'api_user',
-              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
+              username: payload.header.username,
+              password: payload.header.password
             },
-          body: await transformTemplate("EventOnUpdateFirstPaymentStatus", payload.eventData, [])
+          body: await transformTemplate(payload.template.data, payload.eventData, [])
         },
       json: true
     };
     console.log("REQUEST===============>", options.body, "<===============REQUEST");
-    return rp(options);
+    return {message: options.body, func: rp(options)};
   }
 
 }
 
-function updateKYCDetail(payload) {
-  return async () => {
-    let options = {
-      method: 'POST',
-      url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=updateKYCDetail',
-      body:
-        {
-          header:
-            {
-              username: 'api_user',
-              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
-            },
-          body: await transformTemplate("EventOnUpdateKYCDetail-WASL", payload.eventData, [])
-
-        },
-      json: true
-    };
-    console.log("REQUEST===============>", options.body, "<===============REQUEST");
-    return rp(options);
-  }
-
-}
-
-function EjariAvailable(payload) {
-  return async () => {
-    let options = {
-      method: 'POST',
-      url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=terminateContract',
-      body:
-        {
-          header:
-            {
-              username: 'api_user',
-              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
-            },
-          body: await transformTemplate("EventOnEjariAvailable", payload.eventData, [])
-        },
-      json: true
-    };
-    console.log("REQUEST===============>", options.body, "<===============REQUEST");
-    return rp(options);
-  }
-}
-
-function EjariTermination(payload) {
-  return async () => {
-    let options = {
-      method: 'POST',
-      url: 'https://ecservicesqa.wasl.ae/sap/bc/zblckchain?eventName=terminateContract',
-      body:
-        {
-          header:
-            {
-              username: 'api_user',
-              password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
-            },
-          body: await transformTemplate("EventOnEjariTerminationStatus", payload.eventData, [])
-        },
-      json: true
-    };
-// console.log("REQUEST===============>", options.body, "<===============REQUEST");
-// return rp(options);
-    return;
-  }
-}
-
-function UpdateContractStatus(payload) {
+function UpdateContractStatus(contractID) {
   console.log("UpdateContractStatus===============><===============UpdateContractStatus");
   return async () => {
     let options = {
@@ -210,24 +96,23 @@ function UpdateContractStatus(payload) {
           },
           "body": {
             "orgCode": "WASL",
-            "contractID": payload.eventData.contractID
+            "contractID": contractID
           }
         },
       json: true
     };
     console.log("REQUEST===============>", options.body, "<===============REQUEST");
-    return rp(options);
-// return;
+    return {message: options.body, func: rp(options)};
   }
 }
 
-async function getPromise(payload, func, callback) {
-  func().then(response => {
+async function getPromise(payload, {message, func}, callback) {
+  func.then(response => {
     console.log("RESPONSE===============>", response, "<===============RESPONSE");
     callback({
       error: false,
       message: payload.eventData.eventName + " Dispatched",
-      response: response
+      response: {request:message, response}
     })
   }).catch(err => {
     console.log("error : ", err);
