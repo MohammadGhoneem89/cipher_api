020f49c77a4e908a53c1854c4ecf8bb360033808
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-02-28 12:09:11 +0500

    ARRA added

---
+++ struct.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+type MSPList struct {
+	OrgType string `json:"orgType"`
+	MSP     string `json:"MSP"`
+	ID      string `json:"ID"`
+}
+type  AddTenant struct{
+	tenanatfield1 string `json:"OrgIdentifier"`
+tenanat2 string `json:"data"`
+ }
+
+
+type  AssociatePaymentInstrument struct{
+	payment1 string `json:"OrgIdentifier"`
+payment2 string `json:"data"`
+ }
+
+
+type  AssociatePaymentInstrumentType struct{
+	payment1 string `json:"OrgIdentifier"`
+payment2 string `json:"data"`
+ }
+
+
