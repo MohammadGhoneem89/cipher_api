830430a05b796fb784b483c21065dc49ac1e03e9
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-03-14 10:37:12 +0400

    onBoarding moved to core

---
+++ core/mappingFunctions/onBoardingProfileStruct/update.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const onBoarding = require('../../../lib/services/OnBoardingProfileStruct');
+const dates = require('../../../../lib/helpers/dates');
+
+function update(payload, UUIDKey, route, callback, JWToken) {
+    payload.userId = JWToken._id;
+    payload.updatedAt = dates.now;
+    onBoardingUpdate(payload, callback);
+}
+
+function onBoardingUpdate(payload, callback) {
+    onBoarding.update(payload)
+        .then((calendarData) => {
+            let message = {};
+            if(!calendarData){
+                message = {
+                    status: 'ERROR',
+                    errorDescription: 'Payment not Updated',
+                    displayToUser: true
+                }
+            }
+            else{
+                message = {
+                    status: 'OK',
+                    errorDescription: 'Payment Updated Successfully',
+                    displayToUser: true,
+                    newPageURL: '/onBoardingProfile'
+                }
+            }
+            const response = {
+                responseMessage: {
+                    action: payload.action,
+                    data: {
+                        message: message
+                    }
+                }
+            };
+            callback(response);
+        })
+        .catch((err) => {
+            const response = {
+                responseMessage: {
+                    action: payload.action,
+                    data: {
+                        message: {
+                            status: 'ERROR',
+                            errorDescription: 'Payment not Update',
+                            displayToUser: true
+                        },
+                        error: err
+                    }
+                }
+            };
+            callback(response);
+        });
+}
+
+exports.update = update;
+
