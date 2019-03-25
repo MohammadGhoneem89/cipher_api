45375a5e817ac196d9f39e4c3f4b5bee02bc0537
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-03-25 21:53:59 +0400

    GDRFA API handleGDRFAevents

--- applications/WASL/mappingFunctions/events/handlePMevents.js
+++ applications/WASL/mappingFunctions/events/handlePMevents.js
@@ -5,29 +5,16 @@ const config = require('../../../../config');
 const transformTemplate = require('../../../../lib/helpers/transformTemplate');
 const Endpoint = require('../../../../core/Controller/endpoint');
 async function handlePMevents(payload, UUIDKey, route, callback, JWToken) {
- 
- 
- // invoke event blockchain
- // blockchain route api 
- // payload
- //  
- // ServiceURL
-  // let req = _.cloneDeep(payload.eventData);
-  // req = await transformTemplate(payload.template.data, payload.eventData, [])
-  // let _endpoint = new Endpoint(req);
-  // let ServiceURL = '/';
-  // _endpoint.executeEndpoint(payload.endpoint, ServiceURL).then((resp) => {
-  //   if (resp) {
-  //     if (resp.success === false || resp.error === true) {
-  //       throw new Error(resp.message);
-  //     }
-  //     return resp.data;
-  //   }
-  //   return resp;
-  // }).catch((ex) => {
-  //   console.log(ex);
-  //   throw new Error(ex.message);
-  // });a
+
+  console.log('===========handlePMevents payload=======', JSON.stringify(payload))
+
+
+  // invoke event blockchain
+  // blockchain route api 
+  // payload
+  //  
+  // ServiceURL
+
 
 
 
@@ -44,6 +31,11 @@ async function handlePMevents(payload, UUIDKey, route, callback, JWToken) {
       case "UpdatePaymentInstrumentStatus": {
         await UpdateContractStatus(payload.eventData.contractID);
         await getPromise(payload, await createMessage(payload), callback);
+        break;
+      }
+      case "RequestKYC": {
+        RequestKYC(payload);
+       
         break;
       }
       default:
@@ -93,7 +85,74 @@ function UpdateContractStatus(contractID) {
   });
 }
 
+async function RequestKYC(payload) {
+  let req = _.cloneDeep(payload.eventData);
+  req = await transformTemplate(payload.template.data, payload.eventData, [])
+  let _endpoint = new Endpoint(req);
+  let ServiceURL = '/';
+  _endpoint.executeEndpoint(payload.endpoint, ServiceURL).then((resp) => {
+    if (resp) {
+      console.log("=========Response from GDRFA======" + JSON.stringify(resp))
+      
 
+      if (resp.success === false || resp.error === true) {
+        throw new Error(resp.message);
+      }
+    }
+    let mResponse = resp;
+    console.log(mResponse.data.PersonInfo.Address.Passport, "|||||||||||||")
+   UpdateKYCDetail(mResponse);
+    return resp;
+  }).catch((ex) => {
+    console.log(ex);
+    throw new Error(ex.message);
+  });
+}
+
+function UpdateKYCDetail(mResponse) {
+  console.log("UpdateKYCDetail===============><===============UpdateKYCDetail");
+  //  let url = config.get('URLRestInterface') || "http://0.0.0.0/";
+  let message = {
+    method: 'POST',
+    url: 'http://51.140.250.28/API/PR/UpdateKYCDetail',
+    body: {
+      header: 
+      {
+        "username": "gdrfaapi",
+        "password": "b933d03e1b877de6128ad78ab5f96585c99e8321574adb45ff31f597d577acf731d53b17fc42c9a85a197b0b77ef87af6e25f044e689f1b9dbe28fcf73bb074a"  },
+      body: {
+        // "bypassSimu": true,
+         "residenceAddr":mResponse.data.PersonInfo.Address.City.CityDescEN,
+        "contactPersonMobile":"00002030303" ,
+        "nationality": mResponse.data.PersonInfo.Address.Emirate.EmirateDescEN,
+        "dateOfBirth": mResponse.data.PersonInfo.DOB,
+         "natId":mResponse.data.PersonInfo.EmiratesID.EIDNumber,
+         "natIdExpDate":mResponse.data.PersonInfo.EmiratesID.EIDExpiryDate,
+         "poBox": mResponse.data.PersonInfo.Contact.POBox == null ? "1000" : mResponse.data.PersonInfo.Contact.POBox,
+        "passport": {
+            "passportNo": mResponse.data.PersonInfo.Passport.PPNumber,
+            "passportIssueDate": mResponse.data.PersonInfo.Passport.PPIssueDate,
+            "passportExpiryDate": mResponse.data.PersonInfo.Passport.PPExpiryDate,
+            "passportIssuePlace": mResponse.data.PersonInfo.Passport.PPIssuePlaceEN
+        },
+        "phoneNo": mResponse.data.PersonInfo.Contact.HomePhoneNo,
+        "gender": mResponse.data.PersonInfo.Sex == 1 ? "M":"F",
+        "tenantNameEn": mResponse.data.PersonInfo.ApplicantNameEN,
+        "tenantNameAr":  mResponse.data.PersonInfo.ApplicantNameAR,
+        "visaNo": mResponse.data.PersonInfo.ImmigrationFile.FileNumber.toString(),
+        "visaIssueDate": mResponse.data.PersonInfo.ImmigrationFile.FileIssueDate,
+        "visaExpiryDate": mResponse.data.PersonInfo.ImmigrationFile.FileExpiryDate,
+        "visaStatus": mResponse.data.PersonInfo.ImmigrationFile.FileStatus
+      }
+    },
+    json: true
+  };
+  return rp(message).then(result => {
+    console.log("~~~~~~~`",message,"~~~~~~~~~~")
+    console.log("UpdateKYCDetail Response===========>", result, "<===========UpdateKYCDetail Response");
+    return Promise.resolve(true);
+  });
+}
 async function createMessage(payload) {
 
   return await {
