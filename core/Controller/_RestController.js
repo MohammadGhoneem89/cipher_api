80d011b92f0e3d137ac2b713f64ed217aa42ad0b
Bilal Maroof <bilal.maroof@avanzainnovations.com>
2019-04-16 21:30:56 +0400

    upload download changes

--- core/Controller/_RestController.js
+++ core/Controller/_RestController.js
@@ -9,14 +9,7 @@ const _ = require('lodash');
 const apiFilter = ['RenewContract'];
 
 let handleExternalRequest = function (payload, channel, incommingRoute, UUIDKey, responseCallback, JWToken, ConnMQ) {
-  logger.debug({
-    fs: 'RestController.js',
-    func: 'handleExternalRequest'
-  }, "===========Got Message [" + UUIDKey + "]!!!============");
-  logger.debug({ fs: 'RestController.js', func: 'handleExternalRequest' }, JSON.stringify(payload, null, 2));
-  logger.debug({ fs: 'RestController.js', func: 'handleExternalRequest' }, incommingRoute);
-
-  if (apiFilter.indexOf(incommingRoute) >= 0) {
+   if (apiFilter.indexOf(incommingRoute) >= 0) {
     if (payload.body.password || payload.JWToken || payload.JWT) {
       delete payload.body.password;
       delete payload.JWToken;
@@ -34,10 +27,6 @@ let handleExternalRequest = function (payload, channel, incommingRoute, UUIDKey,
   }
 
   let ResponseCaller = function (data, responderMethod) {
-    logger.debug({
-      fs: 'RestController.js',
-      func: 'ResponseCaller'
-    }, "===========Sending Out Message [" + UUIDKey + "]!!!============");
     data = data || {};
     if (data.stack) {
       let error = {
@@ -58,47 +47,15 @@ let handleExternalRequest = function (payload, channel, incommingRoute, UUIDKey,
     responseCallback.send(data);
     return;
   };
-  let route = "";
+  let route = incommingRoute;
   try {
 
-    let routeConfigTemp = routeConfiguration[channel][incommingRoute];
-    if (routeConfigTemp && routeConfigTemp.isRouteOveride && routeConfigTemp.isRouteOveride === true) {
-      if (routeConfigTemp.fieldName && routeConfigTemp.Hierarchy) {
-        try {
-          route = parseDependencyDataAndExtractValue(payload, routeConfigTemp);
-        }
-        catch (err) {
-          responseCallback.send(errorResponse(" Cannot Extract Route Overide Field", UUIDKey));
-          return;
-        }
-
-      }
-      else {
-        responseCallback.send(errorResponse(" Route Configuration invalid for Route Overide", UUIDKey));
-        return;
-      }
-    }
-    else {
-      route = incommingRoute;
-    }
-
     let routeConfig = routeConfiguration[channel][route];
-    logger.debug({
-      fs: 'RestController.js',
-      func: 'handleExternalRequest'
-    }, " [handleExternalRequest] Checking for customMapping");
     if (routeConfig.customMapping === true) {
-      logger.debug({
-        fs: 'RestController.js',
-        func: 'handleExternalRequest'
-      }, " [handleExternalRequest] customMapping returned true");
-      handleCustomMappingFunction(routeConfig.MappingfunctionName, routeConfig.CustomMappingFile, payload, UUIDKey, route, ResponseCaller, JWToken, responseCallback, routeConfig, channel);
+      handleCustomMappingFunction(routeConfig.MappingfunctionName, routeConfig.CustomMappingFile, payload, UUIDKey, route, ResponseCaller, JWToken, responseCallback, routeConfiguration, channel);
       return;
     }
-    logger.debug({
-      fs: 'RestController.js',
-      func: 'handleExternalRequest'
-    }, " [handleExternalRequest] customMapping returned false");
+    responseCallback.send(errorResponse("custom mapping must be true", UUIDKey));
   }
   catch (exp) {
     logger.error(exp);
@@ -124,30 +81,6 @@ function errorResponse(exp, UUIDKey) {
     UUID: UUIDKey
   };
   return response;
-}
-
-function parseDependencyDataAndExtractValue(payload, extConfig) {
-  let locationPlacement = '';
-  if (extConfig.Hierarchy.join('/') === '') {
-    locationPlacement = '';
-  }
-  else {
-    locationPlacement = '/' + extConfig.Hierarchy.join('/');
-  }
-  let obj = "";
-  try {
-    logger.debug({
-      fs: 'RestController.js',
-      func: 'parseAndMapResponse'
-    }, " [parseAndMapResponse] Fetching Location Placement : " + locationPlacement + '/' + extConfig.fieldName);
-    obj = pointer.get(payload, locationPlacement + '/' + extConfig.fieldName);
-
-  }
-  catch (exp) {
-    logger.debug({ fs: 'RestController.js', func: 'parseAndMapResponse' }, " [parseAndMapResponse] Exception: " + exp);
-  }
-  return obj;
-
 }
 
 exports.handleExternalRequest = handleExternalRequest;
