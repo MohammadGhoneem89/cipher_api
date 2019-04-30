a22c6b4a479eca746c0260c2ffe098e962ae5fb7
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-04-30 17:06:36 +0400

    handlers for dpw

--- applications/DPW/mappingFunctions/events/handleDTevents.js
+++ applications/DPW/mappingFunctions/events/handleDTevents.js
@@ -1,44 +1,152 @@
 
 'use strict';
-async function handleSDGevents(payload, UUIDKey, route, callback, JWToken) {
-    try {
-      console.log("<<<<<<<<< Request Recieved for Event >>>>>>>>")
-      console.log(JSON.stringify(payload, null, 2), "========> THIS IS PAYLOAD")
-     
-      switch (payload.eventData.eventName) {
-        
-        case "AddTenant":
-          {
-            return  callback({
-              error:false,
-              message:"AddTenant"
-            })
+let rp = require('request-promise');
+
+async function handleDTevents(payload, UUIDKey, route, callback, JWToken) {
+  try {
+    console.log("<<<<<<<<< Request Recieved for Event >>>>>>>>")
+    console.log(JSON.stringify(payload, null, 2), "========> THIS IS PAYLOAD")
+
+    switch (payload.eventData.eventName) {
+
+      case "EventOnNewRegistration":
+        {
+          if (payload.eventData) {
+            console.log("<<<<  EventOnNewRegistration >>>>>> ")
+            try {
+              await getPromise(payload, createApproveRegistration(payload), callback)
+            } catch (e) {
+              console.log(e);
+            }
             break;
+            //1-  call createApproveReg
+            //2-  let globalID=reponse.globalID
           }
-        case "Logout":
-          {
+        }
+      case "EventOnDataChange":
+        {
+          if (payload.eventData.status == "TERMINATED") {
+            try {
+              await getPromise(payload, endRegistration(payload), callback)
+            } catch (e) {
+              console.log(e);
+            }
+            break;
+            //1-  call ENDREG
+          }
+          else {
+            try {
+              await getPromise(payload, updateRegistration(payload), callback)
+            } catch (e) {
+              console.log(e);
+            }
+            break;
+            //call updateRegistration
+          }
+        }
 
-          }
-        case "UpdateToken":
-          {
-
-          }
-       
-          return "none"
-        default:
-        callback({
-          error:true,
-          message:"invalid case"
+      default:
+        return callback({
+          error: true,
+          message: "invalid case"
         })
-        break;
-      }
     }
-    catch (err) {
-      console.log(err)
-    }
-  }
-  exports.handleSDGevents = handleSDGevents;
 
+  }
+  catch (err) {
+    console.log(err)
+  }
+}
+exports.handleDTevents = handleDTevents;
+
+function createApproveRegistration(payload) {
+  console.log("PAYLOAD=====================> ",
+    payload.eventData, " <=====================PAYLOAD");
+
+  return async () => {
+    console.log("OUTPUT=====================> ",
+      await JSONCOMPARISON("createApproveReg", payload.eventData, []),
+      " <=====================OUTPUT");
+    let options = {
+      method: 'POST',
+      url: 'http://dtdev.dubaitrade.ae/umws/ws/umService.wsdl',
+      body:
+      {
+        header:
+        {
+          username: "",
+          password: ""
+        },
+        //body: await transformTemplate("EventOnRequestEjari", payload.eventData, [])
+
+        body: await JSONCOMPARISONFUNCTION("", '', [])
+      },
+      json: true
+    };
+    console.log("REQUEST===============>", options.body, "<===============REQUEST");
+    return rp(options);
+  }
+}
+
+function updateRegistration(payload) {
+  console.log("PAYLOAD=====================> ",
+    payload.eventData, " <=====================PAYLOAD",
+    payload.eventData.status, "<<<<<<<<<payload status");
+
+  return async () => {
+    console.log("OUTPUT=====================> ",
+      await JSONCOMPARISON("createApproveReg", payload.eventData, []),
+      " <=====================OUTPUT");
+    let options = {
+      method: 'POST',
+      url: 'http://dtdev.dubaitrade.ae/umws/ws/umService.wsdl',
+      body:
+      {
+        header:
+        {
+          username: "",
+          password: ""
+        },
+        //body: await transformTemplate("EventOnRequestEjari", payload.eventData, [])
+
+        body: await JSONCOMPARISONFUNCTION("", '', [])
+      },
+      json: true
+    };
+    console.log("REQUEST===============>", options.body, "<===============REQUEST");
+    return rp(options);
+  }
+}
+
+function endRegistration(payload) {
+  console.log("PAYLOAD=====================> ",
+    payload.eventData, " <=====================PAYLOAD",
+    payload.eventData.status, "<<<<<<<<<payload status");
+
+  return async () => {
+    console.log("OUTPUT=====================> ",
+      await JSONCOMPARISON("createApproveReg", payload.eventData, []),
+      " <=====================OUTPUT");
+    let options = {
+      method: 'POST',
+      url: 'http://dtdev.dubaitrade.ae/umws/ws/umService.wsdl',
+      body:
+      {
+        header:
+        {
+          username: "",
+          password: ""
+        },
+        //body: await transformTemplate("EventOnRequestEjari", payload.eventData, [])
+
+        body: await JSONCOMPARISONFUNCTION("", '', [])
+      },
+      json: true
+    };
+    console.log("REQUEST===============>", options.body, "<===============REQUEST");
+    return rp(options);
+  }
+}
 
 async function getPromise(payload, func, callback) {
   func().then(response => {
--- applications/DPW/mappingFunctions/events/handleREGAUTHevents.js
+++ applications/DPW/mappingFunctions/events/handleREGAUTHevents.js
@@ -1,9 +1,7 @@
 'use strict';
 let rp = require('request-promise')
-const transformTemplate = require('../../../../lib/helpers/transformTemplate');
-const typeData = require('../../../../lib/services/typeData.js')
 
-async function handleDLDevents(payload, UUIDKey, route, callback, JWToken) {
+async function handleREGAUTHevents(payload, UUIDKey, route, callback, JWToken) {
   try {
     console.log("<<<Request Recieved for Event>>>>")
     console.log(JSON.stringify(payload, null, 2));
@@ -11,55 +9,42 @@ async function handleDLDevents(payload, UUIDKey, route, callback, JWToken) {
 
     switch (payload.eventData.eventName) {
 
-      case "TerminateContract": {
+      case "EventOnNewRegistration": {
         try {
-          await getPromise(payload, EventOnTerminateContract(payload), callback);
+          await getPromise(payload, eventOnDataChange(payload), callback);
         } catch (e) {
           console.log(e);
         }
         break;
       }
 
-      case "UpdateFirstPaymentInstrumentStatus": {
+      case "EventOnDataChange": {
         try {
-          await getPromise(payload, EventOnRequestEjari(payload), callback);
+          await getPromise(payload, eventOnDataChange(payload), callback);
         } catch (e) {
           console.log(e);
         }
         break;
       }
-      case "UpdateKYCDetail": {
-        try {
-          await getPromise(payload, EventOnUpdateKYCDetail(payload), callback);
-        } catch (e) {
-          console.log(e);
-        }
-        break;
-      }
-
       default:
         return callback({
           error: true,
           message: "invalid case"
         })
-
     }
   }
   catch (err) {
     console.log(err);
   }
 }
-
-function EventOnUpdateKYCDetail(payload) {
+function eventOnDataChange(payload) {
   console.log("PAYLOAD=====================> ",
     payload.eventData, " <=====================PAYLOAD");
+
   return async () => {
-    console.log("OUTPUT=====================> ",
-      await transformTemplate("EventOnUpdateKYCDetail-DLD", payload.eventData, []),
-      " <=====================OUTPUT");
     let options = {
       method: 'POST',
-      url: 'http://qa.dubailand.gov.ae:8885/v1/TenancyContracts/EventOnUpdateKYCDetail',
+      url: ' http://localhost:9082/API/UR/eventOnDataChange',
       body:
       {
         header:
@@ -67,72 +52,11 @@ function EventOnUpdateKYCDetail(payload) {
           username: "",
           password: ""
         },
-        body: await transformTemplate("EventOnUpdateKYCDetail-DLD", payload.eventData, [])
-      },
-      json: true
-    };
-    console.log("REQUEST===============>", options.body, "<===============REQUEST");
-    return rp(options);
-  }
-
-}
-
-function EventOnRequestEjari(payload) {
-  console.log("PAYLOAD=====================> ",
-    payload.eventData, " <=====================PAYLOAD",
-    payload.eventData.status, "<<<<<<<<<payload status");
-
-  return async () => {
-    let paid = await getStatusfromTypeData();
-    if (payload.eventData.status === paid) {
-      console.log("Contract Status received is PAID  !!!!!!!!!!!!!!!!!!!!!")
-      console.log("OUTPUT=====================> ",
-        await transformTemplate("EventOnRequestEjari", payload.eventData, []),
-        " <=====================OUTPUT");
-      let options = {
-        method: 'POST',
-        url: 'http://qa.dubailand.gov.ae:8885/v1/TenancyContracts/EventOnRequestEjari',
-        body:
-        {
-          header:
-          {
-            username: "",
-            password: ""
-          },
-          body: await transformTemplate("EventOnRequestEjari", payload.eventData, [])
-        },
-        json: true
-      };
-      console.log("REQUEST===============>", options.body, "<===============REQUEST");
-      return rp(options);
-    }
-    else {
-      return Promise.reject("First Payment is not Paid");
-    }
-  }
-
-}
-
-function EventOnTerminateContract(payload) {
-  console.log("PAYLOADY=====================> ",
-    payload.eventData, " <=====================PAYLOADY");
-  return async () => {
-    console.log("OUTPUT=====================> ",
-      await transformTemplate("EventOnTerminateContract", payload.eventData, []),
-      " <=====================OUTPUT");
-    let options = {
-      method: 'POST',
-      url: 'http://qa.dubailand.gov.ae:8885/v1/TenancyContracts/EventOnTerminateContract',
-      body:
-      {
-        header:
-        {
-          username: 'api_user',
-          password: '2c4e9365c231754b208647854e1f608b8db6014d8a28c02a850162963f28ca5b'
-        },
-        body: await transformTemplate("EventOnTerminateContract", payload.eventData, [])
-        // body: EventOnUpdateFirstPaymentStatus
-
+        body: {
+          "unifiedID": "JAFZA_TradeLicenceNumber",
+          "eventData": {},
+          "deltaData": {}
+        }
       },
       json: true
     };
@@ -140,6 +64,7 @@ function EventOnTerminateContract(payload) {
     return rp(options);
   }
 }
+
 
 async function getPromise(payload, func, callback) {
   func().then(response => {
@@ -158,17 +83,5 @@ async function getPromise(payload, func, callback) {
     })
   });
 }
-async function getStatusfromTypeData() {
-  let getStatusfromTypeData = "";
-  let test = await typeData.getTypeData("InstrumentStatus")
-  let JSONtest = JSON.stringify(test.data.InstrumentStatus);
-  let parseJSONtest = JSON.parse(JSONtest)
-  for (let i = 0; i < parseJSONtest.length; i++) {
-    if (parseJSONtest[i].label === "PAID") {
-      //console.log(parseJSONtest[i].label, "JSONVALUEEEEEEEEEEEEEEEEEEEEEEEEE")
-      getStatusfromTypeData = parseJSONtest[i].value;
-      return getStatusfromTypeData;
-    }
-  }
-}
-exports.handleDLDevents = handleDLDevents;
+
+exports.handleREGAUTHevents = handleREGAUTHevents;
