b70e2aa1dba204db5a68de6ec01fc354e9da9bd4
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-06-18 21:48:00 +0500

    typeData func correction

--- applications/etisalat_recon/mappingFunctions/createTypeData/createTypeData.js
+++ applications/etisalat_recon/mappingFunctions/createTypeData/createTypeData.js
@@ -1,21 +1,48 @@
 const typeData = require('../../../../lib/services/typeData');
+const _ = require('lodash');
 
 function createTypeData(payload, UUIDKey, route, callback, JWToken) {
     let structureTypeData;
-    let typeName;
-    let typeNameDetails = [];
+    let getTypeData = [];
+
     for (let i = 0; i < payload.body.data.length; i++) {
-        typeName = "integration_" + payload.body.data[i].type;
-        typeNameDetails.push({
+        getTypeData.push({
+            "typeName": payload.body.data[i].type,
             "label": payload.body.data[i].id,
             "value": payload.body.data[i].description
         })
     }
-    structureTypeData = {
-        action: "typeData",
-        typeName: typeName,
-        typeNameDetails: typeNameDetails
+    const newTypeData = [...new Set(getTypeData.map(it => it.typeName))];
+    let unique;
+    for (let i = 0; i < getTypeData.length; i++) {
+        unique = getTypeData.filter(item => newTypeData.find(obj => item.typeName === obj))
     }
+
+    let newGroup = {}
+    unique.forEach(obj => {
+        newGroup[obj.typeName] ? // check if that array exists or not in newGroup object
+            newGroup[obj.typeName].push({ label: obj.label, value: obj.value })  // just push
+            : (newGroup[obj.typeName] = [],
+                newGroup[obj.typeName].push({ label: obj.label, value: obj.value })) // create a new array and push
+    })
+
+    console.log(newGroup, "newGroup");
+    let keys = Object.keys(newGroup);
+    let typeData = Object.values(newGroup)
+    console.log(keys, "keys")
+    console.log(typeData, "values")
+
+    for (let i = 0; i < Object.keys(newGroup).length; i++) {
+        structureTypeData = {
+            action: "typeData",
+            typeName: keys[i],
+            typeNameDetails: typeData[i]
+        }
+        console.log(structureTypeData, "structureTypeData")
+        insertTypeData(structureTypeData, callback);
+    }
+}
+function insertTypeData(structureTypeData, callback) {
     typeData.insertTypeData(structureTypeData)
         .then(() => {
             const response = {
@@ -37,7 +64,7 @@ function createTypeData(payload, UUIDKey, route, callback, JWToken) {
         .catch((err) => {
             const response = {
                 responseMessage: {
-                    action: payload.action,
+                    action: structureTypeData.action,
                     data: {
                         message: {
                             status: 'ERROR',
@@ -52,6 +79,7 @@ function createTypeData(payload, UUIDKey, route, callback, JWToken) {
             callback(response);
         });
 }
+
 exports.createTypeData = createTypeData;
 
 
