836a6fc70b90694e87ede3f9c7f9fe66a1e19714
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-06-18 22:30:43 +0500

    ...

--- applications/etisalat_recon/mappingFunctions/createTypeData/createTypeData.js
+++ applications/etisalat_recon/mappingFunctions/createTypeData/createTypeData.js
@@ -1,44 +1,50 @@
+'use strict';
 const typeData = require('../../../../lib/services/typeData');
 const _ = require('lodash');
 
+
+
 function createTypeData(payload, UUIDKey, route, callback, JWToken) {
+
+    const data = payload.data;
     let structureTypeData;
     let getTypeData = [];
-
-    for (let i = 0; i < payload.body.data.length; i++) {
-        getTypeData.push({
-            "typeName": payload.body.data[i].type,
-            "label": payload.body.data[i].id,
-            "value": payload.body.data[i].description
-        })
-    }
-    const newTypeData = [...new Set(getTypeData.map(it => it.typeName))];
     let unique;
-    for (let i = 0; i < getTypeData.length; i++) {
-        unique = getTypeData.filter(item => newTypeData.find(obj => item.typeName === obj))
-    }
-
     let newGroup = {}
+    let newTypeData;
+    let keys;
+    let typeData;
+
+
+    data.forEach((object) => {
+        getTypeData.push({
+            "typeName": object.type,
+            "label": object.id,
+            "value": object.description
+        });
+    });
+
+    newTypeData = [...new Set(getTypeData.map(item => item.typeName))];
+    getTypeData.forEach(() => {
+        unique = getTypeData.filter(item => newTypeData.find(obj => item.typeName === obj));
+    })
+
     unique.forEach(obj => {
         newGroup[obj.typeName] ? // check if that array exists or not in newGroup object
             newGroup[obj.typeName].push({ label: obj.label, value: obj.value })  // just push
             : (newGroup[obj.typeName] = [],
-                newGroup[obj.typeName].push({ label: obj.label, value: obj.value })) // create a new array and push
+                newGroup[obj.typeName].push({ label: obj.label, value: obj.value })); // create a new array and push
     })
 
-    console.log(newGroup, "newGroup");
-    let keys = Object.keys(newGroup);
-    let typeData = Object.values(newGroup)
-    console.log(keys, "keys")
-    console.log(typeData, "values")
-
-    for (let i = 0; i < Object.keys(newGroup).length; i++) {
+    keys = Object.keys(newGroup);
+    typeData = Object.values(newGroup);
+    for (let i in Object.keys(newGroup)) {
         structureTypeData = {
             action: "typeData",
             typeName: keys[i],
             typeNameDetails: typeData[i]
         }
-        console.log(structureTypeData, "structureTypeData")
+        console.log(structureTypeData, "structureTypeData");
         insertTypeData(structureTypeData, callback);
     }
 }
@@ -58,7 +64,7 @@ function insertTypeData(structureTypeData, callback) {
                 }
             };
 
-            console.log(JSON.stringify(response));
+            // console.log(JSON.stringify(response));
             callback(response);
         })
         .catch((err) => {
@@ -75,7 +81,7 @@ function insertTypeData(structureTypeData, callback) {
                     }
                 }
             };
-            console.log(JSON.stringify(response))
+            // console.log(JSON.stringify(response))
             callback(response);
         });
 }
