55852aeb6fb47f963b6e2881f0d1526e8fa35fd6
Muhammad Zain <3287275@gmail.com>
2019-06-19 20:56:42 +0400

    code refactored for bankmetada that object is comming.

--- applications/WASL/mappingFunctions/events/handlePMevents.js
+++ applications/WASL/mappingFunctions/events/handlePMevents.js
@@ -24,8 +24,9 @@ async function handlePMevents(payload, UUIDKey, route, callback, JWToken) {
       case "UpdateFirstPaymentInstrumentStatus": {
         await UpdateContractStatus(payload.eventData.contractID);
         let message = await createMessage(payload);
-        let bankMetaData = _.get(message, "body.body.paymentInstruments.[0].bankMetaData", {}); //HACK: For JSON [Object Object] problem with Handlebars.js
-        bankMetaData = JSON.parse(bankMetaData);
+       // let bankMetaData = _.get(message, "body.body.paymentInstruments.[0].bankMetaData", {}); //HACK: For JSON [Object Object] problem with Handlebars.js
+        //bankMetaData = JSON.parse(bankMetaData);
+        let bankMetaData = _.get(payload.eventData, "bankMetaData", {});
         _.set(message, "body.body.paymentInstruments.[0].bankMetaData", bankMetaData);
         await getPromise(payload, message, callback);
         break;
@@ -33,8 +34,9 @@ async function handlePMevents(payload, UUIDKey, route, callback, JWToken) {
       case "UpdatePaymentInstrumentStatus": {
         await UpdateContractStatus(payload.eventData.contractID);
         let message = await createMessage(payload);
-        let bankMetaData = _.get(message, "body.body.paymentInstruments.[0].bankMetaData", {}); //HACK: For JSON [Object Object] problem with Handlebars.js
-        bankMetaData = JSON.parse(bankMetaData);
+        //let bankMetaData = _.get(message, "body.body.paymentInstruments.[0].bankMetaData", {}); //HACK: For JSON [Object Object] problem with Handlebars.js
+       // bankMetaData = JSON.parse(bankMetaData);
+        let bankMetaData = _.get(payload.eventData, "bankMetaData", {});
         _.set(message, "body.body.paymentInstruments.[0].bankMetaData", bankMetaData);
         await getPromise(payload, message, callback);
         break;
