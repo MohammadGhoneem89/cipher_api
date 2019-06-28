e6f21f3883c9a8509274ff6072f0bb40bea17125
MuhammadIbrahim24 <muhammad.ibrahim4724@gmail.com>
2019-06-28 17:31:32 +0400

    governance tool new custom functions added

--- applications/etisalat_recon/mappingFunctions/general/getTaskList.js
+++ applications/etisalat_recon/mappingFunctions/general/getTaskList.js
@@ -3,8 +3,13 @@ const pg = require('../../../../core/api/connectors/postgress');
 const dates = require('../../../../lib/helpers/dates');
 const _ = require('lodash');
 function getTaskList(payload, UUIDKey, route, callback, JWToken) {
-  let queryData = 'SELECT task.taskid, task.status, taskdetails.attributevalue as name FROM task LEFT JOIN taskdetails ON task.taskid=taskDetails.taskid WHERE taskDetails.attributename=\'name\'';
+  let queryData = 'SELECT task.taskid, technician.name, problem, task.status FROM ( SELECT taskid, SUM( CASE WHEN status=\'PROBLEM\' THEN 1 END ) AS problem FROM taskdetails GROUP BY taskid ) details INNER JOIN task ON details.taskid=task.taskid LEFT JOIN technician ON task.technicianid=technician.technicianid';
   let queryCnt = 'SELECT count(*) FROM task WHERE 1=1 ';
+  let queryTaskStatus = 'SELECT status, count(*) FROM task GROUP BY status'
+  let queryAttributeWise = 'SELECT attributename as attribute, SUM( CASE WHEN status=\'PROBLEM\' OR status=\'ACCEPTED\' THEN 1 ELSE 0 END ) AS reported, SUM( CASE WHEN status=\'ACCEPTED\' THEN 1 ELSE 0 END ) AS accepted FROM taskdetails GROUP BY attributename HAVING attributename IN (\'eid\', \'flat\', \'splitter\', \'dropD\', \'x\', \'y\') ';
+  let queryTopFive = 'SELECT actions.technicianid, technician.name as technician, COUNT(attributename) as reported FROM(SELECT* FROM taskdetails INNER JOIN task ON taskdetails.taskid=task.taskid WHERE taskdetails.status=\'ACCEPTED\' AND taskdetails.datetime > \'' + payload.x +'\' ) AS actions INNER JOIN technician ON actions.technicianid = technician.technicianid GROUP BY actions.technicianid, technician.name ORDER BY COUNT(*) DESC LIMIT 5'
+  let queryProblem = 'SELECT actions.technicianid, technician.name, technician.image FROM (SELECT* FROM taskdetails INNER JOIN task ON taskdetails.taskid=task.taskid WHERE taskdetails.status=\'ACCEPTED\' ) AS actions INNER JOIN technician ON actions.technicianid = technician.technicianid GROUP BY actions.technicianid, technician.name, technician.image ORDER BY COUNT(*) DESC LIMIT 1'
+  let queryTopTechnician = 'SELECT actions.technicianid, technician.name, technician.image FROM (SELECT* FROM taskdetails INNER JOIN task ON taskdetails.taskid=task.taskid WHERE taskdetails.status=\'ACCEPTED\' ) AS actions INNER JOIN technician ON actions.technicianid = technician.technicianid GROUP BY actions.technicianid, technician.name, technician.image ORDER BY COUNT(*) DESC LIMIT 1'
   let query = '';
 //   if (payload.searchCriteria && payload.searchCriteria.toDate && payload.searchCriteria.fromDate) {
 //     let fromdate = dates.ddMMyyyyslash(payload.searchCriteria.fromDate);
@@ -25,13 +30,24 @@ function getTaskList(payload, UUIDKey, route, callback, JWToken) {
   let queryCriteria = queryCnt + query;
   let queryCriteriaFull = queryData + query;
   //if (payload.page) { queryCriteriaFull += ` order by "createdAt" desc limit ${payload.page.pageSize} OFFSET ${payload.page.pageSize * (payload.page.currentPageNo - 1)}`; }
-  if (payload.page) { queryCriteriaFull += ` limit ${payload.page.pageSize} OFFSET ${payload.page.pageSize * (payload.page.currentPageNo - 1)}`; }
+  if (payload.page) {
+      queryCriteriaFull += ` limit ${payload.page.pageSize} OFFSET ${payload.page.pageSize * (payload.page.currentPageNo - 1)}`;
+      queryProblem += ` limit ${payload.page.pageSize} OFFSET ${payload.page.pageSize * (payload.page.currentPageNo - 1)}`;
+    }
   pg.connection().then((conn) => {
       console.log("+++",queryCriteria)
       console.log("---",queryCriteriaFull)
+      console.log("===",queryTaskStatus)
+      console.log("###",queryAttributeWise)
+      console.log("***",queryTopFive)
+      console.log("&&&",queryTopTechnician)
     return Promise.all([
       conn.query(queryCriteria, []),
-      conn.query(queryCriteriaFull, [])
+      conn.query(queryCriteriaFull, []),
+      conn.query(queryTaskStatus, []),
+      conn.query(queryAttributeWise, []),
+      conn.query(queryTopFive, []),
+      conn.query(queryTopTechnician, [])
     ]).then((data) => {
       let outVal = [];
       data[1].rows.forEach((elemt) => {
@@ -39,6 +55,7 @@ function getTaskList(payload, UUIDKey, route, callback, JWToken) {
           taskId: elemt.taskid,
           status: elemt.status,
           technicianName: elemt.name,
+          pbTl: (elemt.problem == null ? 0 : elemt.problem) + '/6',
           actions: [{ "value": "1003", "type": "componentAction", "label": "View", "params": "", "iconName": "icon-docs", "URI": ["/etisalat/datastructure"] }]
         });
       });
@@ -51,7 +68,13 @@ function getTaskList(payload, UUIDKey, route, callback, JWToken) {
             "totalRecords": data[0].rows[0].count
           },
           "data": {
-            "searchResult": outVal
+            "searchResult": {
+              taskStatus: data[2].rows,
+              attributewise: data[3].rows,
+              topFive: data[4].rows,
+              topTechnician: data[5].rows,
+              list: outVal
+            }
           }
         }
       };
--- applications/etisalat_recon/mappingFunctions/general/updateTask.js
+++ applications/etisalat_recon/mappingFunctions/general/updateTask.js
@@ -9,7 +9,7 @@ exports.updateTask = function (payload, UUIDKey, route, callback, JWToken) {
     let taskUpdateQuery = 'UPDATE task SET status=\'' + payload.taskStatus + '\' WHERE taskid=\'' + payload.taskId + '\''
     //let taskInsertQuery = 'INSERT INTO task (taskid, status, documents) values (\'' + payload.taskId + '\', \'' + payload.taskStatus  + '\', \'' + JSON.stringify(payload.documents)  + '\')';
     console.log("----", taskUpdateQuery)
-    
+
     payload.taskDetails.forEach((detail) => {
         let updateTaskDetailsQuery = 'UPDATE taskDetails set status=\'' + detail.status + '\' where id=\'' + detail.id + '\'';
         console.log("====", updateTaskDetailsQuery)
@@ -21,20 +21,24 @@ exports.updateTask = function (payload, UUIDKey, route, callback, JWToken) {
                 console.log(ex);
             })
         })
-        
+
 
         //insertTaskDetailsQuery += '(\'' + detail.attributeName + '\', \'' + detail.attributeValue + '\', \'' + detail.correctedValue + '\', \'' + detail.isCorrect + '\', \'' + detail.status + '\', \'' + payload.taskId + '\'), '
     })
     //insertTaskDetailsQuery = insertTaskDetailsQuery.substring(0, insertTaskDetailsQuery.length - 2)
-    
-    let commentInsertQuery = 'INSERT INTO taskcomments (commentdate, username, commenttext, type ,taskId, ecd, reason, attributeid, attributename, status) values '// + payload.comments.commentdate + '\', \'' + payload.comments.username  + '\', \'' + payload.comments.commenttext + '\', \'' + payload.comments.type + '\', \'' + payload.taskId  + '\', \'' + payload.comments.ecd  + '\', \'' + payload.comments.reason  + '\', \'' + payload.comments.attributeid  + '\')';
-    payload.comments.forEach((comment, index) => {
-        if(comment.ecd == undefined) {
-            comment.ecd = 0
-        }
-        commentInsertQuery += '(\'' + comment.commentdate + '\', \'' + comment.username + '\', \'' + comment.commenttext + '\', \'' + comment.type + '\', \'' + payload.taskId + '\', ' + comment.ecd + ', \'' + payload.reason + '\', \'' + comment.attributeid + '\', \'' + comment.attributename + '\', \'' + comment.status + '\'), '
-    })
-    commentInsertQuery = commentInsertQuery.substring(0, commentInsertQuery.length - 2)
+
+    let commentInsertQuery = ''
+    if (payload.comments.length != 0) {
+        commentInsertQuery = 'INSERT INTO taskcomments (commentdate, username, commenttext, type ,taskId, ecd, reason, attributeid, attributename, status) values '// + payload.comments.commentdate + '\', \'' + payload.comments.username  + '\', \'' + payload.comments.commenttext + '\', \'' + payload.comments.type + '\', \'' + payload.taskId  + '\', \'' + payload.comments.ecd  + '\', \'' + payload.comments.reason  + '\', \'' + payload.comments.attributeid  + '\')';
+        payload.comments.forEach((comment, index) => {
+            if (comment.ecd == undefined) {
+                comment.ecd = 0
+            }
+            commentInsertQuery += '(\'' + comment.commentdate + '\', \'' + comment.username + '\', \'' + comment.commenttext + '\', \'' + comment.type + '\', \'' + payload.taskId + '\', ' + comment.ecd + ', \'' + payload.reason + '\', \'' + comment.attributeid + '\', \'' + comment.attributename + '\', \'' + comment.status + '\'), '
+        })
+        commentInsertQuery = commentInsertQuery.substring(0, commentInsertQuery.length - 2)
+    }
+
     //(\'' + payload.taskId + '\', \'' + payload.taskStatus  + '\', \'' + JSON.stringify(payload.documents)  + '\')';
     console.log("++++", commentInsertQuery)
 
