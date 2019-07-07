bf4a04a982a96e8846ccf79e01447bc3a2e86925
MuhammadIbrahim24 <muhammad.ibrahim4724@gmail.com>
2019-07-07 12:47:20 +0400

    technician team and region inserted in db

--- applications/etisalat_recon/mappingFunctions/general/insertTask.js
+++ applications/etisalat_recon/mappingFunctions/general/insertTask.js
@@ -8,12 +8,6 @@ const _ = require('lodash');
 exports.insertTask = function (payload, UUIDKey, route, callback, JWToken) {
     let taskInsertQuery = 'INSERT INTO task (taskid, status, technicianid, documents) values ($1::varchar, $2::varchar, $3::varchar, $4::json)';
     console.log("----", taskInsertQuery)
-    // let insertTaskDetailsQuery = 'INSERT INTO taskDetails (attributeName, attributeValue, correctedValue, isCorrect, status, taskId) values ';
-    // payload.taskDetails.forEach((detail,index) => {
-    //     insertTaskDetailsQuery += '(\'' + detail.attributeName + '\', \'' + detail.attributeValue + '\', \'' + detail.correctedValue + '\', \'' + detail.isCorrect + '\', \'' + detail.status + '\', \'' + payload.taskId + '\'), '
-    // })
-    // insertTaskDetailsQuery = insertTaskDetailsQuery.substring(0, insertTaskDetailsQuery.length - 2)
-    // console.log("====",insertTaskDetailsQuery)
 
     payload.taskDetails.forEach((detail) => {
         let insertTaskDetailsQuery = 'INSERT INTO taskDetails (attributeName, attributeValue, correctedValue, isCorrect, status, taskId) values ($1::varchar, $2::varchar, $3::varchar, $4::bool, $5::varchar, $6::varchar)';
@@ -28,12 +22,8 @@ exports.insertTask = function (payload, UUIDKey, route, callback, JWToken) {
     })
 
     let commentInsertQuery = 'INSERT INTO taskcomments (commentdate, username, commenttext, type ,taskId) values ($1::bigInt, $2::varchar, $3::varchar, $4::varchar, $5::varchar)';
-    let technicianInsertQuery = 'INSERT INTO technician (technicianid, name) values ($1::varchar, $2::varchar) ON CONFLICT DO NOTHING';
-    // payload.comments.forEach((comment,index) => {
-    //     commentInsertQuery += '(\'' + comment.commentdate + '\', \'' + comment.username + '\', \'' + comment.commenttext + '\', \'' + payload.taskId + '\'), '
-    // })
-    // commentInsertQuery = commentInsertQuery.substring(0, commentInsertQuery.length - 2)
-    //(\'' + payload.taskId + '\', \'' + payload.taskStatus  + '\', \'' + JSON.stringify(payload.documents)  + '\')';
+    let technicianInsertQuery = 'INSERT INTO technician (technicianid, name, team, region) values ($1::varchar, $2::varchar, $3::varchar, $4::varchar) ON CONFLICT DO NOTHING';
+    
     console.log("++++", commentInsertQuery)
     console.log("###",technicianInsertQuery)
 
@@ -42,7 +32,7 @@ exports.insertTask = function (payload, UUIDKey, route, callback, JWToken) {
             conn.query(taskInsertQuery, [payload.taskId, payload.taskStatus, payload.techId, JSON.stringify(payload.documents)]),
             // conn.query(insertTaskDetailsQuery, []),
             conn.query(commentInsertQuery, [payload.comments.commentdate, payload.comments.username, payload.comments.commenttext, payload.comments.type, payload.taskId]),
-            conn.query(technicianInsertQuery, [payload.techId, payload.techName])
+            conn.query(technicianInsertQuery, [payload.techId, payload.techName, payload.techTeam, payload.techRegion])
         ]).then((data) => {
             console.log(data);
             let result = _.get(data, 'rowCount', []);
