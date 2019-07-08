1f6ca66665dee3ed2819e9c3e75c88f0d389a8f1
Mohammad Kamran Haider <kamran.haider@avanzainnovations.com>
2019-07-08 15:02:56 +0400

    setup custom

--- applications/DPW/mappingFunctions/events/ccDeployment/CcFiles_Template_Base/typedata.go
+++
@@ -1,9 +0,0 @@
-package main
-
-type TypeDataContractStatus struct {
-	ContractRecorded        string `json:"ContractRecorded"`
-	InstrumentsAssociated   string `json:"InstrumentsAssociated"`
-	CustomerConsentProvided string `json:"CustomerConsentProvided"`
-	Terminating             string `json:"Terminating"`
-	Terminated              string `json:"Terminated"`
-}
