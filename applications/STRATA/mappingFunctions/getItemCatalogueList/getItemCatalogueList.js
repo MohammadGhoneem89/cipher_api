525108152ca11498bba08c822b0c8f3f08b8be9d
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-08-07 18:35:41 +0500

    api updated

--- applications/STRATA/mappingFunctions/getItemCatalogueList/getItemCatalogueList.js
+++ applications/STRATA/mappingFunctions/getItemCatalogueList/getItemCatalogueList.js
@@ -4,34 +4,37 @@ const _ = require('lodash');
 
 
 function getItemCatalogueList(payload, UUIDKey, route, callback, JWToken) {
-    let queryData = `SELECT * FROM "itemcatalogues" WHERE 1=1 `;
+
+
+    console.log(payload,"<------ PAYLOAD")
+    let queryData = `SELECT * FROM itemcatalogues WHERE 1=1`;
     // let queryCnt = 'SELECT count(*) FROM deedtransfers WHERE 1=1 ';
     let query = '';
 
-    if (payload.searchCriteria && payload.searchCriteria.itemCode) {
-        let itemCode = payload.searchCriteria.itemCode;
+    if (payload.body.searchCriteria && payload.body.searchCriteria.itemCode) {
+        let itemCode = payload.body.searchCriteria.itemCode;
         query += ` AND "tranxData" ->> 'itemCode' = '${itemCode}' `;
     }
     //let queryCriteria = queryCnt + query;
     let queryCriteriaFull = queryData + query;
 
-    if (payload.page) { 
-        queryCriteriaFull += ` limit ${payload.page.pageSize} 
-    OFFSET ${payload.page.pageSize * (payload.page.currentPageNo - 1)}`;
+    if (payload.body.page) { 
+        queryCriteriaFull += ` limit ${payload.body.page.pageSize} 
+    OFFSET ${payload.body.page.pageSize * (payload.body.page.currentPageNo - 1)}`;
  }
     console.log("queryCriteriaFull --->",queryCriteriaFull);
 
     pg.connection().then((conn) => {
-        console.log(conn,"<----conn , successfully connected")
+        console.log("<------- Successfully connected --------->")
         return Promise.all([
-           // conn.query(queryCriteria, []),
             conn.query(queryCriteriaFull, [])
         ]).then((data) => {
-            console.log(data,"<----DATA")
-            return callback(response);
+            console.log(data[0].rows,"<----DATA")
+            console.log(data[0].rows.length,"<----length")
+            return callback(data[0].rows);
             });
     }).catch((err) => {
-        console.log(err);
+         console.log("ERROR OCCURRED WHILE EXECUTING QUERY",err);
         return callback(err);
     });
 }
