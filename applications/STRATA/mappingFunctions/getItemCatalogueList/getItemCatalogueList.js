459a68e10975fa4ed0ade41f97b618a73ab850e2
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-08-23 15:39:57 +0500

    api updated

--- applications/STRATA/mappingFunctions/getItemCatalogueList/getItemCatalogueList.js
+++ applications/STRATA/mappingFunctions/getItemCatalogueList/getItemCatalogueList.js
@@ -8,22 +8,34 @@ function getItemCatalogueList(payload, UUIDKey, route, callback, JWToken) {
     let queryData = `SELECT * FROM itemcatalogues WHERE 1=1`;
     let queryCnt = 'SELECT count(*) FROM itemcatalogues WHERE 1=1';
     let query = '';
-
+    
     if (payload.body.searchCriteria && payload.body.searchCriteria.itemCode) {
         let itemCode = payload.body.searchCriteria.itemCode;
         query += ` AND "tranxData" ->> 'itemCode' = '${itemCode}' `;
     }
     if (payload.body.searchCriteria && payload.body.searchCriteria.name) {
-        let name = payload.body.searchCriteria.name;
-        query += ` AND "tranxData" ->> 'name' ='${name}'`;
+       let name = payload.body.searchCriteria.name;
+        //let name= payload.body.searchCriteria.name.replace(/ /gi, '|')
+        console.log("NAME",`'${name}'`)
+        query += ` AND "tranxData" ->> 'name' LIKE '%${name}%'`;
     }
     if (payload.body.searchCriteria && payload.body.searchCriteria.description) {
-        let description = payload.body.searchCriteria.description;
-        query += ` AND "tranxData" ->> 'description' ='${description}'`;
+        //let description= payload.body.searchCriteria.description.replace(/ /gi, '|')
+        let description= payload.body.searchCriteria.description
+        console.log("description",`'${description}'`)
+        query += ` AND "tranxData" ->> 'description' LIKE '%${description}%'`;
     }
     if (payload.body.searchCriteria && payload.body.searchCriteria.material) {
-        let material = payload.body.searchCriteria.material;
-        query += ` AND "tranxData" ->> 'material' ='${material}'`;
+        //let material= payload.body.searchCriteria.material.replace(/ /gi, '|')
+        let material= payload.body.searchCriteria.material
+        console.log("material",`'${material}'`)
+        query += ` AND "tranxData" ->> 'material' LIKE '%${material}%'`;
+    }
+    if (payload.body.searchCriteria && payload.body.searchCriteria.description) {
+        //let description= payload.body.searchCriteria.description.replace(/ /gi, '|')
+        let description= payload.body.searchCriteria.description
+        console.log("description",`'${description}'`)
+        query += ` AND "tranxData" ->> 'description' LIKE '%${description}%'`;
     }
     let queryCriteriaFull = queryData + query;
     let queryCriteria = queryCnt + query;
@@ -35,13 +47,13 @@ function getItemCatalogueList(payload, UUIDKey, route, callback, JWToken) {
     console.log("queryCriteriaFull --->", queryCriteriaFull);
 
     pg.connection().then((conn) => {
-        console.log("Connected to DB")
+        console.log("Connected to DB successfully !")
         return Promise.all([
             conn.query(queryCriteria, []),
             conn.query(queryCriteriaFull, [])
             
         ]).then((data) => {
-            console.log(data,"DATA")
+           // console.log(data,"DATA")
             let result = [];
             if (data) {
                 _.get(_.get(data, '[1]', {}), 'rows', []).forEach((elemt) => {
