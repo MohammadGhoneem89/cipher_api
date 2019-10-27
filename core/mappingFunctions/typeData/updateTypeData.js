c7f727978ac3a97a2369bf6c491c89c9da9bda0a
iqraabdulrauf <iqraabdulrauf01@gmail.com>
2019-10-27 18:07:46 +0400

    LoadConfig() on updateTypedata

--- core/mappingFunctions/typeData/updateTypeData.js
+++ core/mappingFunctions/typeData/updateTypeData.js
@@ -4,48 +4,49 @@
 'use strict';
 
 const typeData = require('../../../lib/services/typeData');
+const apiDef = require('../../mappingFunctions/systemAPI/APIDefination.js')
 
 function updateTypeData(payload, UUIDKey, route, callback, JWToken) {
 
-  payload.userId = JWToken._id;
-  get(payload, callback);
+    payload.userId = JWToken._id;
+    get(payload, callback);
 }
 
 function get(payload, callback) {
 
-  typeData.updateTypeData(payload)
-    .then((typeData) => {
-      const response = {
-        responseMessage: {
-          action: payload.action,
-          data: {
-            message: {
-              status: 'OK',
-              errorDescription: 'typeData updated successfully',
-              displayToUser: true
-            }
-          }
-        }
-      };
-      callback(response);
-    })
-    .catch((err) => {
-      const response = {
-        responseMessage: {
-          action: payload.action,
-          data: {
-            message: {
-              status: 'ERROR',
-              errorDescription: 'typeData not updated',
-              displayToUser: true
-            },
-            error: err.stack || err
-          }
-        }
-      };
-      callback(response);
-    });
+    typeData.updateTypeData(payload)
+        .then((typeData) => {
+            const response = {
+                responseMessage: {
+                    action: payload.action,
+                    data: {
+                        message: {
+                            status: 'OK',
+                            errorDescription: 'typeData updated successfully',
+                            displayToUser: true
+                        }
+                    }
+                }
+            };
+            apiDef.LoadConfig();
+            callback(response);
+        })
+        .catch((err) => {
+            const response = {
+                responseMessage: {
+                    action: payload.action,
+                    data: {
+                        message: {
+                            status: 'ERROR',
+                            errorDescription: 'typeData not updated',
+                            displayToUser: true
+                        },
+                        error: err.stack || err
+                    }
+                }
+            };
+            callback(response);
+        });
 }
 
 exports.updateTypeData = updateTypeData;
-
