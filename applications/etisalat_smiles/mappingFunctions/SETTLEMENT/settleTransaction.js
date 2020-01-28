b46d2ebbe5610cc4f44657bcf8d6d52ebbb95092
iqra abdul rauf <iqraabdulrauf01@gmail.com>
2020-01-29 01:26:27 +0400

    getTrans list | settlement | routes added

--- applications/etisalat_smiles/mappingFunctions/SETTLEMENT/settleTransaction.js
+++ applications/etisalat_smiles/mappingFunctions/SETTLEMENT/settleTransaction.js
@@ -1,16 +1,16 @@
 'use strict';
 const pg = require('../../../../core/api/connectors/postgress');
-const _ = require('lodash');
+const _ = require('./node_modules/lodash');
 const fs = require('fs');
 const { resolve, basename, dirname } = require('path');
 const events = require('events').EventEmitter;
 const emitter = new events.EventEmitter();
 const skipBottomLines = 0;
 const skipLines = 1;
-const Sequelize = require('sequelize');
+const Sequelize = require('./node_modules/sequelize');
 const pgModels = require('../PostgreModel.js')
 var filename = "";
-const { Op } = require("sequelize");
+const { Op } = require("./node_modules/sequelize");
 
 async function settleTransaction(payload, UUIDKey, route, callback, JWToken) {
     let db = pgModels.makeModel('transaction')
