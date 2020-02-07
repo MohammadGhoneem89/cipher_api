e007052b940bfef7a947cbd208462d6197901b19
iqra abdul rauf <iqraabdulrauf01@gmail.com>
2020-02-07 08:36:46 +0500

    code clean

--- applications/etisalat_smiles/mappingFunctions/getAllpossibleConversions.js
+++ applications/etisalat_smiles/mappingFunctions/getAllpossibleConversions.js
@@ -16,52 +16,59 @@ async function getAllPossibleConversionConfig(payload, UUIDKey, route, callback,
         const connection = await pg.connection();
         const queryResult = await connection.query(queryData);
         const pointConvpartners = queryResult.rows;
-        console.log("\n\n >> Query result >> ", pointConvpartners);
-        for (let j = 0; j < pointConvpartners.length; j++) {
-            let conversionsRates = [];
-            let conversion = [];
-            let keys = [];
-            let contractParams = JSON.parse(pointConvpartners[j].contractParams);
-            for (const k in contractParams) { keys.push(k) }; // get all keys from contractParam object
-            for (const key of keys) { //fetch all conversionBillingRates at this key
-                conversionsRates = contractParams[key].conversionBillingRates;
-                for (let i = 0; i < conversionsRates.length; i++) {
-                    conversion.push({
-                        endDate: conversionsRates[i].endDate,
-                        toFromRate: conversionsRates[i].rate,
-                        toToken: conversionsRates[i].sourceToken,
-                        startDate: conversionsRates[i].startDate,
-                        authMethod: contractParams[key].authMethod,
-                        convType: contractParams[key].convType,
-                        toOrg: contractParams[key].withPartnerCode
-
-                    });
-                }
-            }
-            result.push({
-                fromOrg: pointConvpartners[j].partnerCode,
-                fromToken: pointConvpartners[j].fromToken,
-                fromToRate: parseFloat(pointConvpartners[j].fromRate),
-                conversionConfig: conversion
-            });
-        }
-        return callback({
-            messageStatus: "Processed OK!",
-            errorCode: 200,
-            errorDescription: "",
-            timestamp,
-            conversionConfigurations: result
-        });
-    }
-    catch (error) {
-        console.log("Error occurred while executing query! ", error);
-        return callback({
+        const pointConvpartnerlength = pointConvpartners.length;
+        const response = {
             messageStatus: "ERROR",
             errorCode: 201,
             errorDescription: error,
             timestamp,
             conversionConfigurations: [],
-        });
+        }
+        console.log("\n\n >> Query result >> ", pointConvpartners);
+
+        for (let j = 0; j < pointConvpartnerlength; j++) {
+            let pConv = pointConvpartners[j];
+            let conversionsRates = [];
+            let conversion = [];
+            let keys = [];
+            let contractParams = JSON.parse(pConv.contractParams);
+            for (const k in contractParams) { keys.push(k) }; // get all keys from contractParam object
+            for (const key of keys) { //fetch all conversionBillingRates at this key
+                let eachContrctParam = contractParams[key];
+                conversionsRates = eachContrctParam.conversionBillingRates;
+                const conversionsRateslength = conversionsRates.length;
+                for (let i = 0; i < conversionsRateslength; i++) {
+                    let conRt=conversionsRates[i];
+                    conversion.push({
+                        endDate: conRt.endDate,
+                        toFromRate: conRt.rate,
+                        toToken: conRt.sourceToken,
+                        startDate: conRt.startDate,
+                        authMethod: eachContrctParam.authMethod,
+                        convType: eachContrctParam.convType,
+                        toOrg: eachContrctParam.withPartnerCode
+
+                    });
+                }
+            }
+            result.push({
+                fromOrg: pConv.partnerCode,
+                fromToken: pConv.fromToken,
+                fromToRate: parseFloat(pConv.fromRate),
+                conversionConfig: conversion
+            });
+
+        }
+        response.messageStatus = "Processed OK!";
+        response.errorCode = 200;
+        response.errorDescription = "";
+        response.conversionConfigurations = result;
+        return callback(response);
+    }
+    catch (error) {
+        console.log("Error occurred while executing query! ", error);
+        response.conversionConfigurations = [];
+        return callback(response);
     }
 }
 
