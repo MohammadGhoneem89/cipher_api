67104787317acfbf6fe027a58d84a04882a2bc48
nabeel <nabeel.akhtar@avanzainnovations.com>
2020-03-28 16:52:58 +0500

    changes getAllMember + getAllConnected

--- applications/etisalat_smiles/mappingFunctions/partnerSetup/getAllConnectedPartner.js
+++ applications/etisalat_smiles/mappingFunctions/partnerSetup/getAllConnectedPartner.js
@@ -1,4 +1,3 @@
-'use strict';
 const pg = require('../../../../core/api/connectors/postgress');
 const _ = require('lodash');
 const fs = require('fs');
@@ -7,6 +6,7 @@ const events = require('events').EventEmitter;
 const emitter = new events.EventEmitter();
 const skipBottomLines = 0;
 const skipLines = 1;
+const dates = require('../../../../lib/helpers/dates');
 const Sequelize = require('sequelize');
 const pgModels = require('../SETTLEMENT/settlmentModel.js')
 var filename = "";
@@ -14,10 +14,12 @@ const { Op } = require("sequelize");
 
 async function getAllConnectedPartner(payload, UUIDKey, route, callback, JWToken) {
     let response = {
-        "data": {
-            "partners": {
-            }
-        }
+        "messageStatus": "OK",
+        "messageId": UUIDKey,
+        "errorDescription": "",
+        "errorCode": 200,
+        "timestamp": dates.newDate(),
+        partners: {}
     }
 
 
@@ -27,71 +29,149 @@ async function getAllConnectedPartner(payload, UUIDKey, route, callback, JWToken
         }
     }
 
+    // if (payload.body.searchCriteria.startDate && payload.body.searchCriteria.endDate) {
+    //     obj.tranxData['"transactionDate"'] = {
+    //         [Op.gte]: payload.body.searchCriteria.startDate,
+    //         [Op.lte]: payload.body.searchCriteria.endDate
+    //     }
+    // }
+
+    // if (payload.body.searchCriteria.Status) {
+    //     obj.tranxData['"internalStatus"'] = {
+    //         [Op.eq]: payload.body.searchCriteria.Status,
+    //     }
+    // }
+    // if (payload.body.searchCriteria.Partner) {
+    //     obj.tranxData['"partnerCode"'] = {
+    //         [Op.eq]: payload.body.partnerCode
+    //     }
+    // }
+
     let obj1
     // let result = await 
     let result = await db.findAndCountAll({
         where: obj,
         raw: false,
+        limit: payload.body.page.pageSize,
+        offset: (payload.body.page.currentPageNo - 1) * payload.body.page.pageSize
     }).error((err) => {
         console.log("Error " + err)
         return callback(err)
     });
 
-    let tranxData = {}
 
 
-    var d = new Date(0);
+
+
+
+
+    // let rows = _.get(result, 'rows', [])
+    // rows.forEach((row) => {
+
+    //     let actions = [{
+    //         "value": "1003",
+    //         "type": "componentAction",
+    //         "label": "View",
+    //         "params": "",
+    //         "iconName": "icon-docs"
+    //         // "URI": [`/smiles/settlement/view/${row.tranxData.partenerCode}/${row.tranxData.withPartenerCode}`]
+    //     }];
+    //     row.dataValues.actions = actions
+
+    //     row.dataValues.transactionId = `${row.tranxData.key}`
+    // });
+    // response.searchResult[]
+
+    let arr = []
+    let data = {}
+
 
     let { rows: resultRows = [] } = result;
-    let partners = resultRows.map(obj => {
-        tranxData.sourceLoyaltyProgramCode = obj.tranxData.partnerCode
+    // console.log (resultRows)
 
-        for (var key in obj.tranxData.contractParams) {
-            if (obj.tranxData.contractParams[key].isPointConversionPartner == false) {
-                return null
-            }   //set targetLoyalty here   
-            tranxData.targetLoyaltyProgramCode = key
-            var obj1 = obj.tranxData.contractParams[key].conversionBillingRates
-            tranxData.linkingParam = obj.tranxData.contractParams[key].authMethod
-            obj1.forEach((elem) => {
-                if (elem.startDate != "" && elem.endDate == "") {
-                    tranxData.startDate = EpochToDate(elem.startDate)
-                    tranxData.endDate = EpochToDate(elem.endDate)
-                    tranxData.conversionRate = elem.rate
+    let partners = resultRows.map(ptnr => {
+        // console.log(ptnr.tranxData)
+        data.sourceLoyaltyProgramCode = ptnr.tranxData.partnerCode
+        if (ptnr.tranxData.contractParams != null) {
+            for (let key in ptnr.tranxData.contractParams) {
+                if (ptnr.tranxData.contractParams[key].isPointConversionPartner != false) {
+                    data.targetLoyaltyProgramCode = key
+                    data.logo = ptnr.tranxData.contractParams[key].logo || ""
+                    data.linkingParam = ptnr.tranxData.contractParams[key].authType || ""
+                    data.termsAndConditions = ptnr.tranxData.contractParams[key].termsandConditionsAr
+                    data.minConversion = ptnr.tranxData.contractParams[key].minPoints
+                    let obj = ptnr.tranxData.contractParams[key].conversionBillingRates
+                    // console.log("contractParams: ", obj)
+                    obj.forEach(elem => {
+                        data.startDate = EpochToDate(elem.startDate)
+                        data.endDate = EpochToDate(elem.endDate)
+                        data.conversionRate = elem.rate
+                    })
+                    data.feeType = ""
+                    data.feeValue = ""
+                    console.log(data)
+                    arr.push({...data})
+                    console.log(arr)
                 }
-                else {
-                    return null
-                }
 
-
-            })
+            }
         }
+        return data
 
-        tranxData.feeType = ""
-        tranxData.feeValue = ""
+    })
 
-        return tranxData
-    });
 
-    partners = partners.filter(word => word != null)
+    // let partners = resultRows.map(obj => {
+    //     tranxData.sourceLoyaltyProgramCode = obj.tranxData.partnerCode
 
-    console.log(partners);
+    //     // console.log(obj)
+    //     for (var key in obj.tranxData.contractParams) {
+    //         if (obj.tranxData.contractParams[key].isPointConversionPartner == false) {
+    //             return null
+    //         }
+    //         tranxData.termsAndCondtions = obj.tranxData.contractParams[key].termsandConditionsAr
+    //         tranxData.minConversion = obj.tranxData.contractParams[key].minPoints
+    //         //set targetLoyalty here   
+    //         tranxData.targetLoyaltyProgramCode = key
+    //         var obj1 = obj.tranxData.contractParams[key].conversionBillingRates
+    //         tranxData.linkingParam = obj.tranxData.contractParams[key].authMethod
+    //         obj1.forEach((elem) => {
+    //             tranxData.startDate = EpochToDate(elem.startDate)
+    //             tranxData.endDate = EpochToDate(elem.endDate)
+    //             tranxData.conversionRate = elem.rate
 
+
+    //         })
+    //     }
+
+
+    //     tranxData.feeType = ""
+    //     tranxData.feeValue = ""
+
+    //     return tranxData
+    // });
+
+
+    // console.log(partners);
+
+    arr = arr.filter(word => word != null)
 
 
 
     if (result) {
         // result.rows = rows;
-        response.data.partners = partners
+        response.partners = arr
         // response.getAllConnectedPartner.pageData.currentPageNo = payload.body.page.currentPageNo
         // response.getAllConnectedPartner.pageData.pageSize = payload.body.page.pageSize
         return callback(response);
     }
 }
+
 function EpochToDate(epoch) {
     if (epoch < 10000000000)
         epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
     var epoch = epoch + (new Date().getTimezoneOffset() * -1); //for timeZone        
     return new Date(epoch);
 }
+
 exports.getAllConnectedPartner = getAllConnectedPartner
---
+++ applications/etisalat_smiles/mappingFunctions/partnerSetup/getAllMembershipsData.js
@@ -0,0 +1,194 @@
+const pg = require('../../../../core/api/connectors/postgress');
+const _ = require('lodash');
+const fs = require('fs');
+const { resolve, basename, dirname } = require('path');
+const events = require('events').EventEmitter;
+const emitter = new events.EventEmitter();
+const skipBottomLines = 0;
+const skipLines = 1;
+const dates = require('../../../../lib/helpers/dates');
+const Sequelize = require('sequelize');
+const pgModels = require('../SETTLEMENT/settlmentModel.js')
+var filename = "";
+const { Op } = require("sequelize");
+
+async function getAllMembershipsData(payload, UUIDKey, route, callback, JWToken) {
+    let response = {
+        "messageStatus": "OK",
+        "messageId": UUIDKey,
+        "errorDescription": "",
+        "errorCode": 200,
+        "timestamp": dates.newDate(),
+        "OtherPartners": {},
+        "linkedPartners": {}
+    }
+
+
+
+    let db = await pgModels.makeModel('partners')
+    const obj = {
+        tranxData: {
+        }
+    }
+
+    let db_2 = await pgModels.makeModel('memberships')
+
+    const obj2 = {
+        tranxData: {
+        }
+    }
+
+    if (payload.body.sourceLoyaltyProgramCode) {
+        obj2.tranxData['"sourceLoyaltyProgramCode"'] = {
+            [Op.eq]: payload.body.sourceLoyaltyProgramCode
+        }
+    }
+    if (payload.body.membershipNo) {
+        obj2.tranxData['"membershipNo"'] = {
+            [Op.eq]: payload.body.membershipNo
+        }
+    }
+
+    let arr = []
+    let data = {}
+    let myMap = new Map()
+    let linkedarr = []
+
+    let lm = {
+        sourceLoyaltyProgramCode: "",
+        targetLoyaltyProgramCode: "",
+        linkingParam: "",
+        startDate: "",
+        endDate: "",
+        conversionRate: "",
+        termsAndCondtions: "",
+        minConversion: "",
+        feeType: "",
+        feeValue: "",
+        logo: "",
+        OTPLength: "",
+        status: "",
+        points: ""
+    }
+    let lmarr = []
+    let obj1
+    // let result = await 
+    let result = await db.findAndCountAll({
+        where: obj,
+        raw: false
+    }).error((err) => {
+        console.log("Error " + err)
+        return callback(err)
+    });
+
+    let { rows: resultRows = [] } = result;
+    let i = 0
+    // console.log (resultRows)
+
+    let partners = resultRows.map(ptnr => {
+        // console.log(ptnr.tranxData)
+        data.sourceLoyaltyProgramCode = ptnr.tranxData.partnerCode
+        if (ptnr.tranxData.contractParams != null) {
+            for (let key in ptnr.tranxData.contractParams) {
+                if (ptnr.tranxData.contractParams[key].isPointConversionPartner != false) {
+                    data.targetLoyaltyProgramCode = key
+                    data.logo = ptnr.tranxData.contractParams[key].logo || ""
+                    data.linkingParam = ptnr.tranxData.contractParams[key].authType || ""
+                    data.termsAndConditions = ptnr.tranxData.contractParams[key].termsandConditionsAr || ""
+                    data.minConversion = ptnr.tranxData.contractParams[key].minPoints || ""
+                    let obj = ptnr.tranxData.contractParams[key].conversionBillingRates
+                    // console.log("contractParams: ", obj)
+                    obj.forEach(elem => {
+                        data.startDate = EpochToDate(elem.startDate) || ""
+                        data.endDate = EpochToDate(elem.endDate) || ""
+                        data.conversionRate = elem.rate || ""
+                    })
+                    data.feeType = ""
+                    data.feeValue = ""
+                    data.OTPLength = ""
+                    // console.log(data)
+                    arr.push({ ...data })
+                    // console.log(arr)
+                }
+
+            }
+        }
+        return data
+
+    })
+
+    arr = arr.filter(word => word != null)
+    let partArr = []
+
+    arr.forEach(obj => {
+        console.log(obj.targetLoyaltyProgramCode)
+        myMap.set(obj.targetLoyaltyProgramCode, i)
+        i++
+    })
+
+    let linked = await db_2.findAndCountAll({
+        where: obj2,
+        raw: false
+    }).error((err) => {
+        console.log("Error " + err)
+        return callback(err)
+    });
+
+    let { rows: resultRows_link = [] } = linked;
+
+    let linkedData = {}
+    let linkedArr = []
+    let linkMem = resultRows_link.map(link => {
+        for (key in link.tranxData.linkInfo) {
+            if (link.tranxData.linkInfo[key].status == "A" || link.tranxData.linkInfo[key].status == "P") {
+                linkedData.status = link.tranxData.linkInfo[key].status || ""
+                linkedData.targetLoyaltyProgramCode = link.tranxData.targetOrgCode
+                // linkedData.targetLoyaltyProgramCode = key
+                linkedData.points = link.tranxData.linkInfo[key].points || 0
+                linkedArr.push({ ...linkedData })
+            }
+        }
+        return linkedData
+    })
+
+    console.log(linkedArr)
+
+    linkedArr.forEach(obj => {
+        let index = myMap.get(obj.targetLoyaltyProgramCode)
+        console.log(typeof (index))
+        if (!(typeof (index) === 'undefined')) {
+            let temp = arr[index]
+            temp.points = obj.points || 0
+            temp.status = obj.status
+            partArr.push({ ...temp })
+            if (index >= 0) {
+                console.log("here")
+                arr.splice(index, 1)
+                console.log(index + "\n\n")
+                console.log(arr + "\n\n")
+                myMap.set(obj.targetLoyaltyProgramCode, -1)
+            }
+        }
+    })
+
+
+
+    if (result) {
+        // result.rows = rows;
+        response.OtherPartners = arr
+        response.linkedPartners = partArr
+
+        // response.getAllConnectedPartner.pageData.currentPageNo = payload.body.page.currentPageNo
+        // response.getAllConnectedPartner.pageData.pageSize = payload.body.page.pageSize
+        return callback(response);
+    }
+}
+
+function EpochToDate(epoch) {
+    if (epoch < 10000000000)
+        epoch *= 1000; // convert to milliseconds (Epoch is usually expressed in seconds, but Javascript uses Milliseconds)
+    var epoch = epoch + (new Date().getTimezoneOffset() * -1); //for timeZone        
+    return new Date(epoch);
+}
+
+exports.getAllMembershipsData = getAllMembershipsData
