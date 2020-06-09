3444e042b815eab36b6d9dc10ed30fc75b224a45
Mohammad Musa Chaudhary <musa.chaudhary@avanzainnovations.com>
2020-06-09 12:32:11 +0400

    added task search and task search details api

--- applications/Task/mappingFunctions/search.js
+++ applications/Task/mappingFunctions/search.js
@@ -10,35 +10,95 @@ const connector = require('../../../core/api/client');
 async function search(payload, UUIDKey, route, callback, JWToken) {
    console.log("-----------------response")
    console.log("payload",payload.body.searchCriteria)
-   let queryTask=`SELECT * FROM public.tasks`
-   let queryCnt = `SELECT count(*) FROM public.tasks WHERE 1=1`;
-   let query=''
+   let queryTask=`SELECT * FROM public.tasks  WHERE 1=1 `
+   let queryCnt = `SELECT count(*) FROM public.tasks WHERE 1=1 `;
+  // let queryCnt = `SELECT count(*) FROM public.tasks WHERE 1=1`;
+    let query=''
     if(payload.body.searchCriteria.task_type){
-      task_type = payload.body.searchCriteria.task_type;
-      query += ` AND task_type = '${task_type}' `;
+      let task_type = payload.body.searchCriteria.task_type;
+      console.log("-----------------task_type",task_type)
+      query += ` AND task_type = '${task_type}' `
     }
     if(payload.body.searchCriteria.api_url){
-      api_url = payload.body.searchCriteria.api_url;
-      query += ` AND api_url = '${api_url}' `;
+      let api_url = payload.body.searchCriteria.api_url;
+      console.log("-----------------api_url", api_url)
+      query += ` AND api_url = '${api_url}' ` 
     }
-    if(payload.body.searchCriteria.schedule_time){
-      schedule_time = payload.body.searchCriteria.schedule_time;
-      query += ` AND schedule_time = '${schedule_time}' `;
+    if(payload.body.searchCriteria.schedule_time){ 
+      let schedule_time = payload.body.searchCriteria.schedule_time;
+      console.log("-----------------schedule_time",schedule_time)  
+      query += ` AND INT8(schedule_time) <= '${schedule_time}' `    
+
+      //query += ` AND schedule_time = '${schedule_time}' `    
     }
     if(payload.body.searchCriteria.status){
-      status = payload.body.searchCriteria.status;
-      query += ` AND status = '${status}' `;
+      let status = payload.body.searchCriteria.status;
+      console.log("-----------------status",status)
+      query += ` AND status = '${status}' `
     }
     let queryCriteriaFull = queryTask + query;
     let queryCriteria = queryCnt + query;
 
-    console.log("-------------------------------query",query)
+    if (payload.body.page && payload.body.page.pageSize && payload.body.page.currentPageNo) {
+      queryCriteriaFull += ` limit ${payload.body.page.pageSize} 
+  OFFSET ${payload.body.page.pageSize * (payload.body.page.currentPageNo - 1)}`;
+  }
+    // pg.connection().then(async (conn)=>{
+    //   res = await conn.query(queryCriteriaFull,[])
+    //   console.log("res----------------------",res)
+    //   return callback(res)
+    // })
+ //   console.log("-------------------------------query",query)
     console.log("-------------------------------queryCriteriaFull",queryCriteriaFull)
-    console.log("-------------------------------queryCriteria",queryCriteria)
+   // console.log("-------------------------------queryCriteria",queryCriteria)
+
+    pg.connection().then((conn) => {
+       console.log("Connected to DB successfully !")
+       return Promise.all([
+           conn.query(queryCriteria , []),
+           conn.query(queryCriteriaFull, []),
+           //conn.query(query2,[]),
+       ]).then((data) => {
+           let result = data[1].rows
+           let total=data[0].rows[0].count
+          // console.log("obj1----------------------",data)
+
+          console.log("obj1----------------------",data[0].rows[0].count)
+          console.log("obj2----------------------",data[1].rows)
+          let response = {
+            "searchTask": {
+                "pageData": {
+                    "pageSize": payload.body.page ? payload.body.page.pageSize : undefined,
+                    "currentPageNo": payload.body.page ? payload.body.page.currentPageNo : 1,
+                     "totalRecords": total
+                },
+
+                "searchResult": result
+            }
+        };
+
+           return callback(response)
+           
+       });
+   }).catch((err) => {
+       console.log("Error occurred while executing query ", err);
+       return callback(err);
+   });
+
+    // pg.connection().then((conn)=>{
+    //   return Promise.all([
+    //     conn.query(queryCriteriaFull,[]),
+    //     conn.query(queryCriteria,[])
+    //   ]).then((data)=>{
+    //     console.log("data----------------------")
+    //   })
+    // })
+
+    
 
 
 
-   return callback(payload.body.searchCriteria)
+   //return callback(payload.body.searchCriteria)
    
  
  
--- applications/Task/mappingFunctions/searchTaskDetails.js
+++ applications/Task/mappingFunctions/searchTaskDetails.js
@@ -12,6 +12,66 @@ async function searchTaskDetails(payload, UUIDKey, route, callback, JWToken) {
   //let id=payload.body.id
   //let query=`SELECT * from logs`
    let query=`SELECT * from logs where task_id=${task_id}`
+   let queryCnt = `SELECT count(*) FROM public.logs where task_id=${task_id}`;
+   //let query=`SELECT * from logs where log_id='${id}'`
+   console.log("-----------------query",payload.body.page)
+   //let queryCriteria = queryCnt + query;
+
+   if (payload.body.page && payload.body.page.pageSize && payload.body.page.currentPageNo) {
+      query += ` limit ${payload.body.page.pageSize} 
+  OFFSET ${payload.body.page.pageSize * (payload.body.page.currentPageNo - 1)}`;
+  }
+  // let res 
+   pg.connection().then(async (conn) => {
+      console.log("Connected to DB successfully !")
+      return Promise.all([
+         conn.query(query),
+         conn.query(queryCnt)
+      ]).then((data)=>{
+         let result=data[0].rows
+         let total=data[1].rows[0].count
+         console.log(data[0].rows)
+         console.log(data[1].rows[0].count)
+         let response={
+            "TaskDetails": {
+               "pageData": {
+                   "pageSize": payload.body.page ? payload.body.page.pageSize : undefined,
+                   "currentPageNo": payload.body.page ? payload.body.page.currentPageNo : 1,
+                    "totalRecords": total
+               },
+
+               "searchResult": result
+           }
+         }
+
+         return callback(response)
+      })
+   //   res = await conn.query(query, []);
+   //   console.log("-----------------response",res.rows)
+   //   let searchTaskDetailsData= {searchTaskDetailsData:res.rows}
+   //   return callback(searchTaskDetailsData)
+  });
+   
+
+   
+  
+    
+}
+exports.searchTaskDetails = searchTaskDetails;
+
+
+
+
+
+
+
+/**
+ * async function searchTaskDetails(payload, UUIDKey, route, callback, JWToken) {
+   console.log("-----------------payload.body.task_id",payload.body.task_id)
+  let  task_id=payload.body.task_id
+  //let id=payload.body.id
+  //let query=`SELECT * from logs`
+   let query=`SELECT * from logs where task_id=${task_id}`
    //let query=`SELECT * from logs where log_id='${id}'`
    console.log("-----------------query",query)
 
@@ -22,14 +82,9 @@ async function searchTaskDetails(payload, UUIDKey, route, callback, JWToken) {
      console.log("-----------------response",res.rows)
      let searchTaskDetailsData= {searchTaskDetailsData:res.rows}
      return callback(searchTaskDetailsData)
-  });
-   
-
-   
-  
-    
+  });   
 }
-exports.searchTaskDetails = searchTaskDetails;
+ */
 
  // rp(options).then(val=>{console.log("val-------------",val)}).catch(err=> console.log("err-------------",err))
     //console.log("---------------res",res)
