9e521f892442a6ba410e41a6e44c81ce41622241
Farhan Rafaqat <farhan.rafaqat@avanzainnovations.com>
2020-09-21 11:16:41 +0500

    Get Document Type List

--- test.js
+++
@@ -1,8 +0,0 @@
-let str = `select * from apipayload where 1=1  [AND nice='{{cook}}']  [AND nice2='{{cook2}}']  AND ([nice='{{cook}}'] OR [nice='{{cook}}']) `;
-while (str.match(/\[(.*?)\]/) && str.match(/\[(.*?)\]/).length > 0) {
-  let matches = str.match(/\[(.*?)\]/);
-  console.log(matches[0], 'replaced')
-  str = str.replace(/\[(.*?)\]/, matches[1]);
-  console.log(str)
-}
-console.log(str);
